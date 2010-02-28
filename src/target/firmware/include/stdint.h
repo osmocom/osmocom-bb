e03b8de3d49120a07a3feb27b60118fbc6f3f4df
Harald Welte <laforge@gnumonks.org>
2010-02-28 20:01:47 +0100

    ensure osmocom-bb builds with older gcc-3.x based toolchains

---
+++ src/target/firmware/include/stdint.h
@@ -0,0 +1,16 @@
+#ifndef _STDINT_H
+#define _STDINT_H
+
+/* some older toolchains (like gnuarm-3.x) don't provide a C99
+   compliant stdint.h yet, so we define our own here */
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+
+typedef signed long int32_t;
+typedef unsigned long uint32_t;
+
+#endif
