26810bd363c936c9fca0b138d01ab0a46dd900a6
Harald Welte <laforge@gnumonks.org>
2010-02-28 19:29:59 +0100

    add missing async.h file (belongs to 0da792f2522eef475d0beb1690cd02c341415f82)

---
+++ src/target/firmware/include/layer1/async.h
@@ -0,0 +1,19 @@
+#ifndef _L1_ASYNC_H
+#define _L1_ASYNC_H
+
+/* safely enable a message into the L1S TX queue */
+void l1a_txq_msgb_enq(struct llist_head *queue, struct msgb *msg);
+
+/* request a RACH request at the next multiframe T3 = fn51 */
+void l1a_rach_req(uint8_t fn51, uint8_t ra);
+
+/* Enable a repeating multiframe task */
+void l1a_mftask_enable(enum mframe_task task);
+
+/* Disable a repeating multiframe task */
+void l1a_mftask_disable(enum mframe_task task);
+
+/* Initialize asynchronous part of Layer1 */
+void l1a_init(void);
+
+#endif
