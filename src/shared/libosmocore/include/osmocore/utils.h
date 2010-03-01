5a1ef4361860c2c9dc986b134cd6f1037758ed2e
Harald Welte <laforge@gnumonks.org>
2010-03-01 21:58:31 +0100

    Import value_string utilities and some RSL stuff from OpenBSC

--- src/shared/libosmocore/include/osmocore/utils.h
+++ src/shared/libosmocore/include/osmocore/utils.h
@@ -3,4 +3,15 @@
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
+#include <stdint.h>
+
+struct value_string {
+	unsigned int value;
+	const char *str;
+};
+
+const char *get_value_string(const struct value_string *vs, uint32_t val);
+int get_string_value(const struct value_string *vs, const char *str);
+
+
 #endif
