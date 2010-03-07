ed1b30b2e8f5422a1eecb2008b61d2ff1aecc038
Harald Welte <laforge@gnumonks.org>
2010-03-07 13:32:43 +0100

    fix stdint.h to remove bogus warnings of printf format strings

--- src/target/firmware/include/stdint.h
+++ src/target/firmware/include/stdint.h
@@ -10,7 +10,7 @@ typedef unsigned char uint8_t;
 typedef signed short int16_t;
 typedef unsigned short uint16_t;
 
-typedef signed long int32_t;
-typedef unsigned long uint32_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
 
 #endif
