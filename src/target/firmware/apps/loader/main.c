f871b070ffa12c7afe304af676f9f54c000986e1
Ingo Albrecht <prom@berlin.ccc.de>
2010-04-18 09:55:36 +0200

    loader: commented startup behaviour

--- src/target/firmware/apps/loader/main.c
+++ src/target/firmware/apps/loader/main.c
@@ -112,13 +112,13 @@ loader_send_init(uint8_t dlci) {
 
 flash_t the_flash;
 
-extern void puts_asm(char *s);
 extern void putchar_asm(uint32_t c);
 
 static const uint8_t phone_ack[]     = { 0x1b, 0xf6, 0x02, 0x00, 0x41, 0x03, 0x42 };
 
 int main(void)
 {
+	/* Simulate a compal loader saying "ACK" */
 	int i = 0;
 	for(i = 0; i < sizeof(phone_ack); i++) {
 		putchar_asm(phone_ack[i]);
