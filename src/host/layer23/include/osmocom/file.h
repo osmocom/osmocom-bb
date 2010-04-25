96e479645f3f3e85b46a9c5840c1e036a4197de9
Andreas Eversberg <jolly@eversberg.eu>
2010-04-25 17:11:47 +0200

    Wrapper for file access.
    If file access is used to store network informations and user settings in the
    /etc/ directory. Later it can be used to store them in the EEPROM of the
    target.

---
+++ src/host/layer23/include/osmocom/file.h
@@ -0,0 +1,33 @@
+#ifndef _OSMOCOM_FILE_H
+#define _OSMOCOM_FILE_H
+
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#define OSMOCOM_CONFDIR "/etc/osmocom/"
+#define OSMOCOM_PERM 0644
+#define OSMOCOM_FILE FILE
+static inline OSMOCOM_FILE *osmocom_fopen(const char *name, const char *mode)
+{
+	char filename[strlen(OSMOCOM_CONFDIR) + strlen(name) + 1];
+
+	if (mode[0] != 'r') {
+		int rc;
+
+		rc = mkdir(OSMOCOM_CONFDIR, OSMOCOM_PERM);
+		if (rc < 1 && errno != EEXIST)
+			return NULL;
+	}
+
+	strcpy(filename, OSMOCOM_CONFDIR);
+	strcat(filename, name);
+	
+	return fopen(filename, mode);
+}
+#define osmocom_fread fread
+#define osmocom_fwrite fwrite
+#define osmocom_feof feof
+#define osmocom_fclose fclose
+
+#endif /* _OSMOCOM_FILE_H */
