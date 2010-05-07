452bc184700841744884e4675db5e3bc520198d5
Harald Welte <laforge@gnumonks.org>
2010-05-07 16:21:43 +0200

    cosmetic/whitespace change

--- src/target/firmware/layer1/prim_rach.c
+++ src/target/firmware/layer1/prim_rach.c
@@ -88,9 +88,9 @@ static int l1s_tx_rach_resp(__unused uint8_t p1, __unused uint8_t burst_id,
 
 /* sched sets for uplink */
 const struct tdma_sched_item rach_sched_set_ul[] = {
-	SCHED_ITEM(l1s_tx_rach_cmd, 1, 0),				SCHED_END_FRAME(),
-									SCHED_END_FRAME(),
-	SCHED_ITEM(l1s_tx_rach_resp, 1, 0),				SCHED_END_FRAME(),
+	SCHED_ITEM(l1s_tx_rach_cmd, 1, 0),	SCHED_END_FRAME(),
+						SCHED_END_FRAME(),
+	SCHED_ITEM(l1s_tx_rach_resp, 1, 0),	SCHED_END_FRAME(),
 	SCHED_END_SET()
 };
 
