cfb545aebbd909b55f7889360cf8cfc3e2f7debe
Harald Welte <laforge@gnumonks.org>
2010-06-03 21:30:57 +0200

    [GPRS] BSSGP: Fix formatting of BSSGP TX STATUS

--- openbsc/src/gprs/gprs_bssgp_util.c
+++ openbsc/src/gprs/gprs_bssgp_util.c
@@ -101,7 +101,8 @@ int bssgp_tx_status(uint8_t cause, uint16_t *bvci, struct msgb *orig_msg)
 	struct bssgp_normal_hdr *bgph =
 			(struct bssgp_normal_hdr *) msgb_put(msg, sizeof(*bgph));
 
-	DEBUGPC(DBSSGP, "BSSGP: TX STATUS, cause=%s\n", bssgp_cause_str(cause));
+	DEBUGP(DBSSGP, LOGL_NOTICE, "BSSGP BVCI=%u Tx STATUS, cause=%s\n",
+		bvci ? *bvci : 0, bssgp_cause_str(cause));
 	msgb_nsei(msg) = msgb_nsei(orig_msg);
 	msgb_bvci(msg) = 0;
 
