8c1f25e6276058b4ae96b37138b2ad1f91c1bb7e
Harald Welte <laforge@gnumonks.org>
2010-06-24 17:10:46 +0200

    [firmware] fix code that iterates over constructors
    the first element after __CTORS_LIST__ contains the number of constructor
    callbacks in the following array, so we have to skip it.

--- src/target/firmware/lib/ctors.c
+++ src/target/firmware/lib/ctors.c
@@ -1,11 +1,15 @@
 
 /* iterate over list of constructor functions and call each element */
-void do_global_ctors(const char *ctors_start, const char *ctors_end)
+void do_global_ctors(const char *_ctors_start, const char *ctors_end)
 {
 	typedef void (*func_ptr)(void);
-	func_ptr *func;
+	func_ptr *func, *ctors_start = (func_ptr *) _ctors_start;
 
-	for (func = (func_ptr *) ctors_start;
+	/* skip the first entry, as it contains the number of
+	 * constructors which we don't use */
+	ctors_start++;
+
+	for (func = ctors_start;
 	     *func && (func != (func_ptr *) ctors_end); func++)
 		(*func)();
 }
