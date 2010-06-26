77c53d1e667af32c3e5636a24d721dd257485808
Andreas.Eversberg <jolly@eversberg.eu>
2010-06-26 16:38:07 +0000

    [layer23] Committed missing vty.h of last commit.

---
+++ src/host/layer23/include/osmocom/vty.h
@@ -0,0 +1,19 @@
+#ifndef OSMOCOM_VTY_H
+#define OSMOCOM_VTY_H
+
+#include <osmocom/osmocom_data.h>
+#include <osmocom/vty/vty.h>
+#include <osmocom/vty/buffer.h>
+#include <osmocom/vty/command.h>
+
+enum ms_vty_node {
+	MS_NODE = _LAST_OSMOVTY_NODE + 1,
+	TESTSIM_NODE,
+};
+
+enum node_type ms_vty_go_parent(struct vty *vty);
+int ms_vty_init(void);
+extern void vty_notify(struct osmocom_ms *ms, const char *fmt, ...) __attribute__ ((format (printf, 2, 3)));
+
+#endif
+
