231444b548df8aff616f314ebc6135588b0ffec3
Harald Welte <laforge@gnumonks.org>
2010-07-12 09:17:54 +0200

    remove old / modified copy of libosmocore (will be replaced with new)

--- src/shared/libosmocore/include/osmocom/vty/logging.h
+++
@@ -1,7 +0,0 @@
-#ifndef _VTY_LOGGING_H
-#define _VTY_LOGGING_H
-
-#define LOGGING_STR	"Configure log message to this terminal\n"
-#define FILTER_STR	"Filter log messages\n"
-
-#endif /* _VTY_LOGGING_H */
