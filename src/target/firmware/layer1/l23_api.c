a4b1b1bcc18a06a089235c8ada3d6f89fc66d125
Sylvain Munaut <tnt@246tNt.com>
2010-07-25 19:18:26 +0200

    layer1/l23_api: Fix the L3_MSG_SIZE to include l1ctl header size
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/target/firmware/layer1/l23_api.c
+++ src/target/firmware/layer1/l23_api.c
@@ -43,7 +43,7 @@
 
 /* the size we will allocate struct msgb* for HDLC */
 #define L3_MSG_HEAD 4
-#define L3_MSG_SIZE (sizeof(struct l1ctl_info_dl)+sizeof(struct l1ctl_data_ind) + L3_MSG_HEAD)
+#define L3_MSG_SIZE (sizeof(struct l1ctl_hdr)+sizeof(struct l1ctl_info_dl)+sizeof(struct l1ctl_data_ind) + L3_MSG_HEAD)
 
 void l1_queue_for_l2(struct msgb *msg)
 {
