da65f025807e2af7da420ad33f09b4e11a5b9020
Andreas.Eversberg <jolly@eversberg.eu>
2010-08-21 15:09:48 +0000

    [layer23] Header file for last commit

--- src/host/layer23/include/osmocom/bb/common/l1ctl.h
+++ src/host/layer23/include/osmocom/bb/common/l1ctl.h
@@ -26,6 +26,12 @@ int l1ctl_tx_dm_est_req_h0(struct osmocom_ms *ms, uint16_t band_arfcn,
 int l1ctl_tx_dm_est_req_h1(struct osmocom_ms *ms, uint8_t maio, uint8_t hsn,
 	uint16_t *ma, uint8_t ma_len, uint8_t chan_nr, uint8_t tsc);
 
+/* Transmit L1CTL_DM_FREQ_REQ */
+int l1ctl_tx_dm_freq_req_h0(struct osmocom_ms *ms, uint16_t band_arfcn,
+	uint8_t tsc, uint16_t fn);
+int l1ctl_tx_dm_freq_req_h1(struct osmocom_ms *ms, uint8_t maio, uint8_t hsn,
+	uint16_t *ma, uint8_t ma_len, uint8_t tsc, uint16_t fn);
+
 /* Transmit L1CTL_DM_REL_REQ */
 int l1ctl_tx_dm_rel_req(struct osmocom_ms *ms);
 
