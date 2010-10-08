1a4ea5b4364b756276da5d378d81e3b15a1d5f84
Sylvain Munaut <tnt@246tNt.com>
2010-10-08 15:09:16 +0200

    codec: Add bit ordering tables from specs for HR,FR,EFR & AMR
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

---
+++ include/osmocom/codec/codec.h
@@ -0,0 +1,19 @@
+#ifndef _OSMOCOM_CODEC_H
+#define _OSMOCOM_CODEC_H
+
+#include <stdint.h>
+
+extern uint16_t gsm610_bitorder[];	/* FR */
+extern uint16_t gsm620_bitorder[];	/* HR */
+extern uint16_t gsm660_bitorder[];	/* EFR */
+
+extern uint16_t gsm690_12_2_bitorder[];	/* AMR 12.2  kbits */
+extern uint16_t gsm690_10_2_bitorder[];	/* AMR 10.2  kbits */
+extern uint16_t gsm690_7_95_bitorder[];	/* AMR  7.95 kbits */
+extern uint16_t gsm690_7_4_bitorder[];	/* AMR  7.4  kbits */
+extern uint16_t gsm690_6_7_bitorder[];	/* AMR  6.7  kbits */
+extern uint16_t gsm690_5_9_bitorder[];	/* AMR  5.9  kbits */
+extern uint16_t gsm690_5_15_bitorder[];	/* AMR  5.15 kbits */
+extern uint16_t gsm690_4_75_bitorder[];	/* AMR  4.75 kbits */
+
+#endif /* _OSMOCOM_CODEC_H */
