c0500693a6e5cb764a0c80a61d0166dbbcfa3cad
Andreas.Eversberg <jolly@eversberg.eu>
2010-10-24 12:23:05 +0000

    [layer23] Added debugging for GPS at logging.c

--- src/host/layer23/src/common/logging.c
+++ src/host/layer23/src/common/logging.c
@@ -114,6 +114,12 @@ static const struct log_info_cat default_categories[] = {
 		.color = "\033[0;35m",
 		.enabled = 1, .loglevel = LOGL_DEBUG,
 	},
+	[DGPS] = {
+		.name = "DGPS",
+		.description = "GPS",
+		.color = "\033[1;35m",
+		.enabled = 1, .loglevel = LOGL_DEBUG,
+	},
 };
 
 const struct log_info log_info = {
