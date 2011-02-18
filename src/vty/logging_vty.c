6b431033b3118b1893a94735292143f6c67cfd20
Harald Welte <laforge@gnumonks.org>
2011-02-18 20:39:36 +0100

    LOGGING: Add missing 'nat' subsystem

--- src/vty/logging_vty.c
+++ src/vty/logging_vty.c
@@ -144,7 +144,7 @@ DEFUN(logging_prnt_timestamp,
 }
 
 /* FIXME: those have to be kept in sync with the log levels and categories */
-#define VTY_DEBUG_CATEGORIES "(rll|cc|mm|rr|rsl|nm|sms|pag|mncc|inp|mi|mib|mux|meas|sccp|msc|mgcp|ho|db|ref|gprs|ns|bssgp|llc|sndcp|isup|m2ua|pcap|all)"
+#define VTY_DEBUG_CATEGORIES "(rll|cc|mm|rr|rsl|nm|sms|pag|mncc|inp|mi|mib|mux|meas|sccp|msc|mgcp|ho|db|ref|gprs|ns|bssgp|llc|sndcp|isup|m2ua|pcap|nat|all)"
 #define CATEGORIES_HELP	\
 	"A-bis Radio Link Layer (RLL)\n"			\
 	"Layer3 Call Control (CC)\n"				\
@@ -174,6 +174,7 @@ DEFUN(logging_prnt_timestamp,
 	"ISDN User Part (ISUP)\n"				\
 	"SCTP M2UA\n"						\
 	"Trace message IO\n"					\
+	"BSC NAT\n"						\
 	"Global setting for all subsytems\n"
 
 #define VTY_DEBUG_LEVELS "(everything|debug|info|notice|error|fatal)"
