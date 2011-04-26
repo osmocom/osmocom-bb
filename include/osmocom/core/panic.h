240f01cfa738d353520dd96536031ffbe6054bcc
Pablo Neira Ayuso <pablo@gnumonks.org>
2011-04-26 03:07:46 +0200

    include: fix missing extern in osmo_panic* declarations
    This patch adds the missing extern to osmo_panic* declarations.

--- include/osmocom/core/panic.h
+++ include/osmocom/core/panic.h
@@ -5,7 +5,7 @@
 
 typedef void (*osmo_panic_handler_t)(const char *fmt, va_list args);
 
-void osmo_panic(const char *fmt, ...);
-void osmo_set_panic_handler(osmo_panic_handler_t h);
+extern void osmo_panic(const char *fmt, ...);
+extern void osmo_set_panic_handler(osmo_panic_handler_t h);
 
 #endif
