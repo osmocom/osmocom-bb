68b1574257fdbf4b06d225c116cfa6a2a6929965
Harald Welte <laforge@gnumonks.org>
2011-05-22 21:47:29 +0200

    socket: use listen() and SO_REUSEADDR, new osmo_sock_init_ofd() function
    osmo_sock_init_ofd() is a wrapper around osmo_sock_init() which will
    take care of initializing and registering a 'struct osmo_fd' for the
    newly-created socket.

--- include/osmocom/core/socket.h
+++ include/osmocom/core/socket.h
@@ -8,6 +8,9 @@ struct sockaddr;
 int osmo_sock_init(uint16_t family, uint16_t type, uint8_t proto,
 		   const char *host, uint16_t port, int connect0_bind1);
 
+int osmo_sock_init_ofd(struct osmo_fd *ofd, int family, int type, int proto,
+			const char *host, uint16_t port, int connect0_bind1);
+
 int osmo_sock_init_sa(struct sockaddr *ss, uint16_t type,
 		      uint8_t proto, int connect0_bind1);
 
