osmocom/osmocom-bb

commit 79599ba0eb7a9aeaeb9dfbbf4881f0633724a471

tlv: Make tlv parser arrays 256 entries wide to prevent overflow on 0xff
If we encounter a tag with 0xFF, we overflow our existing tlv_parse
array definitions.

Warning: this breaks ABI

include/osmocom/gsm/tlv.h
@@ -236,11 +236,11 @@ struct tlv_def {
 };
 
 struct tlv_definition {
-	struct tlv_def def[0xff];
+	struct tlv_def def[265];
 };
 
 struct tlv_parsed {
-	struct tlv_p_entry lv[0xff];
+	struct tlv_p_entry lv[265];
 };
 
 extern struct tlv_definition tvlv_att_def;

src/gsm/Makefile.am
@@ -1,6 +1,6 @@
 # This is _NOT_ the library release version, it's an API version.
 # Please read Chapter 6 "Library interface versions" of the libtool documentation before making any modification
-LIBVERSION=0:0:0
+LIBVERSION=1:0:0
 
 INCLUDES = $(all_includes) -I$(top_srcdir)/include
 AM_CFLAGS = -fPIC -Wall

commit e0aa5bb9f758efdac73d89e270c86ed73b6dbe9a

gsm 04.80: fix typo (256, not 265!)
This was introduced by me in commit
79599ba0eb7a9aeaeb9dfbbf4881f0633724a471

Thanks to Alexander Huemer for detecting it

include/osmocom/gsm/tlv.h
@@ -236,11 +236,11 @@ struct tlv_def {
 };
 
 struct tlv_definition {
-	struct tlv_def def[265];
+	struct tlv_def def[256];
 };
 
 struct tlv_parsed {
-	struct tlv_p_entry lv[265];
+	struct tlv_p_entry lv[256];
 };
 
 extern struct tlv_definition tvlv_att_def;