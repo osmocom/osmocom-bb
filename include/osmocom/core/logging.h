osmocom/osmocom-bb

commit 95f7eb288c4b8b69d61fa8d68957fb21f09e11e5

remove DLRSL and DLNM, we don't need them
They are not used anywhere in our libraries, so they should be defined
by the respective applications

include/osmocom/core/logging.h
@@ -43,9 +43,7 @@ void logp(int subsys, char *file, int line, int cont, const char *format, ...) _
 #define DLMUX		-4
 #define DLMI		-5
 #define DLMIB		-6
-#define DLRSL		-7
-#define DLNM		-8
-#define OSMO_NUM_DLIB	9
+#define OSMO_NUM_DLIB	7
 
 struct log_category {
 	uint8_t loglevel;

src/logging.c
@@ -94,18 +94,6 @@ static const struct log_info_cat internal_cat[OSMO_NUM_DLIB] = {
 		.description = "A-bis Input Driver for B-Channels (voice)",
 		.enabled = 0, .loglevel = LOGL_NOTICE,
 	},
-	[INT2IDX(DLRSL)] = {
-		.name = "DRSL",
-		.description = "A-bis Radio Siganlling Link (RSL)",
-		.color = "\033[1;35m",
-		.enabled = 1, .loglevel = LOGL_NOTICE,
-	},
-	[INT2IDX(DLNM)] = {
-		.name = "DNM",
-		.description = "A-bis Network Management / O&M (NM/OML)",
-		.color = "\033[1;36m",
-		.enabled = 1, .loglevel = LOGL_INFO,
-	},
 };
 
 /* You have to keep this in sync with the structure loglevel_strs. */

commit 2af435986f14b14171c8581fff090ea9fb41fc61

fix off-by-one error in OSMO_NUM_DLIB
This bug was introduced in 95f7eb288c4b8b69d61fa8d68957fb21f09e11e5 and
it caused a segfault on 'write terminal'

include/osmocom/core/logging.h
@@ -43,7 +43,7 @@ void logp(int subsys, char *file, int line, int cont, const char *format, ...) _
 #define DLMUX		-4
 #define DLMI		-5
 #define DLMIB		-6
-#define OSMO_NUM_DLIB	7
+#define OSMO_NUM_DLIB	6
 
 struct log_category {
 	uint8_t loglevel;