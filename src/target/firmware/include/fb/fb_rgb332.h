12d7ca237fd7dce4a37497428694d9b6729184e7
Christian Vogel <vogelchr@vogel.cx>
2012-01-30 14:57:44 +0100

    Calypso FB: RGB332 to RGB565 conversion function.

--- src/target/firmware/include/fb/fb_rgb332.h
+++ src/target/firmware/include/fb/fb_rgb332.h
@@ -24,4 +24,24 @@ extern int fb_rgb332_putstr(char *str,int maxwidth);
 
 extern struct fb_rgb332 *fb_rgb332;
 
+/* this convenience function can be used if you choose to
+ * back a RGB565 display with a RGB332 framebuffer to conserve
+ * ARM memory. It converts a rgb332 value to rgb565 as indicated
+ * in the comments. */
+
+static inline uint16_t
+rgb332_to_565(uint8_t rgb332){
+
+	uint8_t red   =  (rgb332 & 0xe0) >> 5 ; // rrr. .... -> .... .rrr
+	uint8_t green = ((rgb332 & 0x1c) >> 2); // ...g gg.. -> .... .ggg
+	uint8_t blue  =   rgb332 & 0x03;        // .... ..bb -> .... ..bb
+
+	red   = (red   << 2) | (red >> 1);                /* .....210 -> ...21021 */
+	green = (green << 3) | (green);                   /* .....210 -> ..210210 */
+	blue  = (blue  << 3) | (blue << 1) | (blue >> 1); /* ......10 -> ...10101 */
+
+	/* rrrrrggg gggbbbbb */
+	return (red << 11) | (green << 5) | blue;
+}
+
 #endif
