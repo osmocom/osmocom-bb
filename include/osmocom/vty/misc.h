738f13395d1d8a005011e45ebbc6a869bbcfd9a6
Holger Hans Peter Freyther <zecke@selfish.org>
2012-03-24 18:26:24 +0100

    vty: Add a function to write the current config to a file.

--- include/osmocom/vty/misc.h
+++ include/osmocom/vty/misc.h
@@ -7,4 +7,7 @@
 void vty_out_rate_ctr_group(struct vty *vty, const char *prefix,
                             struct rate_ctr_group *ctrg);
 
+int osmo_vty_write_config_file(const char *filename);
+int osmo_vty_save_config_file(void);
+
 #endif
