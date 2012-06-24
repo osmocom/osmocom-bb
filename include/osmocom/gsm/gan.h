c17f57053802c8b151df38169b73eef804ce2b3e
Harald Welte <laforge@gnumonks.org>
2012-06-24 21:52:07 +0200

    ganc: add value_strings for GAN message type and protocol discriminator

---
+++ include/osmocom/gsm/gan.h
@@ -0,0 +1,9 @@
+#ifndef _OSMO_GSM_GAN_H
+#define  _OSMO_GSM_GAN_H
+
+#include <osmocom/core/utils.h>
+
+extern const struct value_string gan_msgt_vals[];
+static const struct value_string gan_pdisc_vals[];
+
+#endif
