6ad25f290e8cb697115a4170d6fbff66f3128528
Tobias Engel <t-openbsc@tobias.org>
2012-10-24 17:52:53 +0200

    Add "extern" keywords
    Without the "extern" keyword the variables in this header file will be
    seen as empty definitions when compiled on OSX.

--- include/osmocom/gsm/abis_nm.h
+++ include/osmocom/gsm/abis_nm.h
@@ -14,10 +14,10 @@
 enum abis_nm_msgtype;
 enum gsm_phys_chan_config;
 
-const enum abis_nm_msgtype abis_nm_reports[4];
-const enum abis_nm_msgtype abis_nm_no_ack_nack[3];
-const enum abis_nm_msgtype abis_nm_sw_load_msgs[9];
-const enum abis_nm_msgtype abis_nm_nacks[33];
+extern const enum abis_nm_msgtype abis_nm_reports[4];
+extern const enum abis_nm_msgtype abis_nm_no_ack_nack[3];
+extern const enum abis_nm_msgtype abis_nm_sw_load_msgs[9];
+extern const enum abis_nm_msgtype abis_nm_nacks[33];
 
 extern const struct value_string abis_nm_obj_class_names[];
 extern const struct value_string abis_nm_adm_state_names[];
@@ -26,7 +26,7 @@ const char *abis_nm_nack_cause_name(uint8_t cause);
 const char *abis_nm_nack_name(uint8_t nack);
 const char *abis_nm_event_type_name(uint8_t cause);
 const char *abis_nm_severity_name(uint8_t cause);
-const struct tlv_definition abis_nm_att_tlvdef;
+extern const struct tlv_definition abis_nm_att_tlvdef;
 const char *abis_nm_opstate_name(uint8_t os);
 const char *abis_nm_avail_name(uint8_t avail);
 const char *abis_nm_test_name(uint8_t test);
