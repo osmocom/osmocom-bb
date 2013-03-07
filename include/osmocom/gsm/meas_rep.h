e128f4663104ed64e33e362cff2566f36d65e658
Harald Welte <laforge@gnumonks.org>
2013-03-07 18:36:32 +0000

    add new gsm/meas_rep.h file for shared/common measurement report stuff

---
+++ include/osmocom/gsm/meas_rep.h
@@ -0,0 +1,29 @@
+#ifndef _OSMO_MEAS_REP_H
+#define _OSMO_MEAS_REP_H
+
+#include <stdint.h>
+
+/* RX Level and RX Quality */
+struct gsm_rx_lev_qual {
+	uint8_t rx_lev;
+	uint8_t rx_qual;
+};
+
+/* unidirectional measumrement report */
+struct gsm_meas_rep_unidir {
+	struct gsm_rx_lev_qual full;
+	struct gsm_rx_lev_qual sub;
+};
+
+enum meas_rep_field {
+	MEAS_REP_DL_RXLEV_FULL,
+	MEAS_REP_DL_RXLEV_SUB,
+	MEAS_REP_DL_RXQUAL_FULL,
+	MEAS_REP_DL_RXQUAL_SUB,
+	MEAS_REP_UL_RXLEV_FULL,
+	MEAS_REP_UL_RXLEV_SUB,
+	MEAS_REP_UL_RXQUAL_FULL,
+	MEAS_REP_UL_RXQUAL_SUB,
+};
+
+#endif
