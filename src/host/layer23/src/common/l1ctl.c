134c95c81749d7b0fd65a528eea7ac0441aa7454
Andreas Eversberg <jolly@eversberg.eu>
2013-12-23 11:14:46 +0100

    Bad frame indication at l1ctrl.c: Replaced printf-hack by LOGP

--- src/host/layer23/src/common/l1ctl.c
+++ src/host/layer23/src/common/l1ctl.c
@@ -244,8 +244,7 @@ static int rx_ph_data_ind(struct osmocom_ms *ms, struct msgb *msg)
 	}
 
 	if (dl->fire_crc >= 2) {
-printf("Dropping frame with %u bit errors\n", dl->num_biterr);
-		LOGP(DL1C, LOGL_NOTICE, "Dropping frame with %u bit errors\n",
+		LOGP(DRR, LOGL_NOTICE, "Bad frame frame with %u bit errors\n",
 			dl->num_biterr);
 		msgb_free(msg);
 		return 0;
