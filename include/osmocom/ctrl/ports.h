c7947ed1f6fe8a9113451fbd1e58078a98ee4912
Harald Welte <laforge@gnumonks.org>
2014-08-21 12:17:11 +0200

    New <osmocom/ctrl/ports.h> file listing tcp ports for CTRL interface

---
+++ include/osmocom/ctrl/ports.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/* TCP port numbers used for CTRL interfaces in osmocom projects */
+
+#define OSMO_CTRL_PORT_NITB_BSC	4249
+#define OSMO_CTRL_PORT_BSC_NAT	4250
+#define OSMO_CTRL_PORT_SGSN	4251
