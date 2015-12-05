90e614f7ccb0902e6ddb510bb10f78eb14a13d94
Harald Welte <laforge@gnumonks.org>
2015-12-05 23:38:18 +0100

    remove our internal copy of talloc, use system libtalloc
    Shipping our own private copy of talloc was a good idea in 2008,
    when it was not readily available on most target platforms.  Today,
    the situation is quite different, as it is a standard library on
    major Linux distributions.

---
+++ include/osmocom/core/talloc.h
@@ -0,0 +1,4 @@
+/* Convenience wrapper.  libosmocore used to ship its own internal copy of
+ * talloc, before libtalloc became a standard component on most systems */
+#pragma once
+#include <talloc.h>
