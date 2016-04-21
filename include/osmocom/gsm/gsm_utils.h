402b9b055538737838ce112a99a75401aa4f14c6
Max <msuraev@sysmocom.de>
2016-04-21 14:58:30 +0200

    Addditional classmark check against A5/4 support
    Add classmark check function according to GSM 04.08 Chapter 10.5.1.7
    Fix interface to use unsigned for unsigned value.

--- include/osmocom/gsm/gsm_utils.h
+++ include/osmocom/gsm/gsm_utils.h
@@ -119,12 +119,24 @@ int rxlev2dbm(uint8_t rxlev);
 uint8_t dbm2rxlev(int dbm);
 
 /* According to GSM 04.08 Chapter 10.5.1.6 */
-static inline int ms_cm2_a5n_support(uint8_t *cm2, int n) {
+static inline int ms_cm2_a5n_support(uint8_t *cm2, unsigned n) {
 	switch (n) {
 		case 0: return 1;
 		case 1: return (cm2[0] & (1<<3)) ? 0 : 1;
 		case 2: return (cm2[2] & (1<<0)) ? 1 : 0;
 		case 3: return (cm2[2] & (1<<1)) ? 1 : 0;
+		default:
+			return 0;
+	}
+}
+
+/* According to GSM 04.08 Chapter 10.5.1.7 */
+static inline int ms_cm3_a5n_support(uint8_t *cm3, unsigned n) {
+	switch (n) {
+		case 4: return (cm3[0] & (1<<0)) ? 1 : 0;
+		case 5: return (cm3[0] & (1<<1)) ? 1 : 0;
+		case 6: return (cm3[0] & (1<<2)) ? 1 : 0;
+	        case 7: return (cm3[0] & (1<<3)) ? 1 : 0;
 		default:
 			return 0;
 	}
