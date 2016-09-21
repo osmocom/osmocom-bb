56155c16b91428c4838df1d6010cdb7acdb540ce
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-09-21 23:37:37 +0200

    jenkins.sh: output all test logs when 'make check' failed
    Assume that cat-testlogs.sh from osmo-ci is installed in $HOME/osmo-ci/scripts,
    and call from jenkins.sh upon 'make check' failure.
    
    Change-Id: I18a08e7ade1a53783d5a4171fe825f61b49457be

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+export PATH="$PATH:$HOME/osmo-ci/scripts"
 
 set -ex
 
 autoreconf --install --force
 ./configure --enable-static
 $MAKE $PARALLEL_MAKE
-$MAKE distcheck
+$MAKE distcheck \
+  || cat-testlogs.sh
