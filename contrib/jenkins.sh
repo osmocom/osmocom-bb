a23817622b28cb1969a73ffd36da501eb29b9cd7
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2016-09-30 01:25:45 +0200

    configure: add --enable-sanitize, use in jenkins.sh
    --enable-sanitize adds address sanitizer CFLAGS/CPPFLAGS to the entire build.
    Also pass UBSAN_OPTIONS to the test suite run (only has effect during runtime).
    
    Add this flag to jenkins.sh's configure step. To ensure that we get the
    sanitize results, add 'make check' to jenkins to catch sanitize failures;
    
    Keep 'make distcheck' without ASAN; it has its own configure which omits
    --enable-sanitize. This way we test both with and without ASAN.
    
    Change-Id: Idf7f46fa048608c2951f2473cb528f6c8dc2681d

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -3,7 +3,8 @@
 set -ex
 
 autoreconf --install --force
-./configure --enable-static
-$MAKE $PARALLEL_MAKE
+./configure --enable-static --enable-sanitize
+$MAKE $PARALLEL_MAKE check \
+  || cat-testlogs.sh
 $MAKE distcheck \
   || cat-testlogs.sh
