84ac308411474aa2e1f6bb063f1dd0906e8f33b2
Harald Welte <laforge@gnumonks.org>
2017-01-15 19:15:14 +0100

    Add an endian.h file as one step to build against later libosmocore

---
+++ src/target/firmware/include/endian.h
@@ -0,0 +1,12 @@
+#ifndef __ENDIAN_H
+#define	__ENDIAN_H
+
+#include <sys/cdefs.h>
+#include <sys/_types.h>
+#include <machine/_endian.h>
+
+#define __BYTE_ORDER	__LITTLE_ENDIaN
+#define	_QUAD_HIGHWORD	1
+#define	_QUAD_LOWWORD	0
+
+#endif /* __ENDIAN_H__ */
