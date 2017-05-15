ad92f0835341330097f4be62ee2257abcd69977c
Harald Welte <laforge@gnumonks.org>
2017-05-15 17:41:15 +0200

    remove old, outdated copy of libosmocore
    Change-Id: I8dc5adc9e9249577bd824a9d85a80003c53949e0

--- src/shared/libosmocore/include/osmocom/core/backtrace.h
+++
@@ -1,7 +0,0 @@
-#ifndef _OSMO_BACKTRACE_H_
-#define _OSMO_BACKTRACE_H_
-
-void osmo_generate_backtrace(void);
-void osmo_log_backtrace(int subsys, int level);
-
-#endif
--- src/shared/libosmocore/include/osmocom/gsm/gan.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OSMO_GSM_GAN_H
-#define  _OSMO_GSM_GAN_H
-
-#include <osmocom/core/utils.h>
-
-extern const struct value_string gan_msgt_vals[];
-static const struct value_string gan_pdisc_vals[];
-
-#endif
