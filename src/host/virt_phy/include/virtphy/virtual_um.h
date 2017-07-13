70dd7e5b75b1e7e008545a615d33d1134cf42339
Harald Welte <laforge@gnumonks.org>
2017-07-13 17:15:14 +0200

    VIRT-PHY: Use IPv4 multicast groups for private / local scope
    The addresses in the original code make little sense:
    * 224.0.0.1 is "All systems on this subnet" and not routed
      outside the local ethernet segment
    * 225.0.0.1 is in a RESERVED range that shouldn't be used
    
    Change-Id: I8e3acd745e65a6cfa70b681a440da6a59a1ed0b5

--- src/host/virt_phy/include/virtphy/virtual_um.h
+++ src/host/virt_phy/include/virtphy/virtual_um.h
@@ -4,10 +4,15 @@
 #include <osmocom/core/msgb.h>
 #include "osmo_mcast_sock.h"
 
+/* We use multicast group addresses from the 239.192.0.0/14 rage, as
+ * those are designated by RFC2365 as "IPv4 Organization Local Scope,
+ * "... the space from which an organization should allocate sub-
+ *  ranges when defining scopes for private use." */
+
 #define VIRT_UM_MSGB_SIZE	256
-#define DEFAULT_MS_MCAST_GROUP "224.0.0.1"
+#define DEFAULT_MS_MCAST_GROUP	"239.193.23.1"
 #define DEFAULT_MS_MCAST_PORT 4729 /* IANA-registered port for GSMTAP */
-#define DEFAULT_BTS_MCAST_GROUP "225.0.0.1"
+#define DEFAULT_BTS_MCAST_GROUP	"239.193.23.2"
 #define DEFAULT_BTS_MCAST_PORT 4729 /* IANA-registered port for GSMTAP */
 
 struct virt_um_inst {
