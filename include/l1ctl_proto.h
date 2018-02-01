9fa291dd2ba880be0a07c0c3fff1ce537bbee4d1
Sylvain Munaut <tnt@246tNt.com>
2018-02-01 12:05:28 +0100

    l1ctl_proto.h: define burst indication messages
    One burst indication message carries a single raw burst, forwarded
    to the higher layers from DSP. The burst bits are hard-bits (0 or 1)
    coming from demodulator, packed to 14.5 bytes. This is why we call
    them 'raw' - no bit correction nor deciphering is performed.
    
    Related: OS#1672
    Change-Id: I75e33c38accdf2a0af961c89836c5e7a3a056bda

--- include/l1ctl_proto.h
+++ include/l1ctl_proto.h
@@ -56,6 +56,7 @@ enum {
 	L1CTL_TRAFFIC_REQ,
 	L1CTL_TRAFFIC_CONF,
 	L1CTL_TRAFFIC_IND,
+	L1CTL_BURST_IND,
 
 	/* configure TBF for uplink/downlink */
 	L1CTL_TBF_CFG_REQ,
@@ -293,6 +294,19 @@ struct l1ctl_pm_req {
 			uint16_t band_arfcn_to;
 		} range;
 	};
+} __attribute__((packed));
+
+#define BI_FLG_DUMMY (1 << 4)
+#define BI_FLG_SACCH (1 << 5)
+
+struct l1ctl_burst_ind {
+	uint32_t frame_nr;
+	uint16_t band_arfcn;    /* ARFCN + band + ul indicator               */
+	uint8_t chan_nr;        /* GSM 08.58 channel number (9.3.1)          */
+	uint8_t flags;          /* BI_FLG_xxx + burst_id = 2LSBs             */
+	uint8_t rx_level;       /* 0 .. 63 in typical GSM notation (dBm+110) */
+	uint8_t snr;            /* Reported SNR >> 8 (0-255)                 */
+	uint8_t bits[15];       /* 114 bits + 2 steal bits. Filled MSB first */
 } __attribute__((packed));
 
 /* a single L1CTL_PM response */
