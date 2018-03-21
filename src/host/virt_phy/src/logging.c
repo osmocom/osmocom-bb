f9ac7eb36ebc97c68a98c700f801015c0fc9c8ad
Pau Espin Pedrol <pespin@sysmocom.de>
2018-03-21 17:23:09 +0100

    virt_phy: Add missing gprs related entries to l1ctlPrimNames
    Change-Id: Ia59e22cda9cf5e25b5e2b1fe38f8ec3937b16f80

--- src/host/virt_phy/src/logging.c
+++ src/host/virt_phy/src/logging.c
@@ -56,7 +56,12 @@ static const char* l1ctlPrimNames[] = {
         "L1CTL_NEIGH_PM_IND",
         "L1CTL_TRAFFIC_REQ",
         "L1CTL_TRAFFIC_CONF",
-        "L1CTL_TRAFFIC_IND"};
+        "L1CTL_TRAFFIC_IND",
+        "L1CTL_TBF_CFG_REQ",
+        "L1CTL_TBF_CFG_CONF",
+        "L1CTL_DATA_TBF_REQ",
+        "L1CTL_DATA_TBF_CONF"
+};
 
 static const struct log_info_cat default_categories[] = {
 	[DL1C] = {
