5408068c0ed90d07400e4c2a1a3e143e79f13afc
Vadim Yanitskiy <axilirator@gmail.com>
2018-08-17 12:34:10 +0700

    mobile/mncc.h: migrate some GSM_TCH* messages from LCR
    Change-Id: If8f391b171155f73da4ed4ba76a49b6ef63bb89f

--- src/host/layer23/include/osmocom/bb/mobile/mncc.h
+++ src/host/layer23/include/osmocom/bb/mobile/mncc.h
@@ -108,6 +108,8 @@ struct gsm_call {
 
 #define GSM_TCHF_FRAME		0x0300
 #define GSM_TCHF_FRAME_EFR	0x0301
+#define GSM_TCHH_FRAME		0x0302
+#define GSM_TCH_FRAME_AMR	0x0303
 
 #define GSM_MAX_FACILITY	128
 #define GSM_MAX_SSVERSION	128
