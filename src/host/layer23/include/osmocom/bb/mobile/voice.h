7900ba1a13a727de069fde3f7c3cd662bed4ef3e
Vadim Yanitskiy <axilirator@gmail.com>
2018-08-17 12:05:41 +0700

    mobile: voice.h: use '#pragma once' include guard
    Change-Id: I024e13c0f66ae1e42b0c8e2ff77874fcb883e85a
    Related: OS#5599

--- src/host/layer23/include/osmocom/bb/mobile/voice.h
+++ src/host/layer23/include/osmocom/bb/mobile/voice.h
@@ -1,7 +1,4 @@
-#ifndef _voice_h
-#define _voice_h
+#pragma once
 
 int gsm_voice_init(struct osmocom_ms *ms);
 int gsm_send_voice(struct osmocom_ms *ms, struct gsm_data_frame *data);
-
-#endif /* _voice_h */
