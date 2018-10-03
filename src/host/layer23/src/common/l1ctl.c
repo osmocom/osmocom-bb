d9a3877e2c88e347455941d677edaa3027a1876f
Vadim Yanitskiy <axilirator@gmail.com>
2018-10-04 03:44:48 +0700

    layer23/l1ctl.c: cosmetic: drop useless includes
    Change-Id: I16f595110faf27444175864384ce6800a90e10da

--- src/host/layer23/src/common/l1ctl.c
+++ src/host/layer23/src/common/l1ctl.c
@@ -34,10 +34,7 @@
 #include <osmocom/core/logging.h>
 #include <osmocom/core/timer.h>
 #include <osmocom/core/msgb.h>
-#include <osmocom/gsm/tlv.h>
 #include <osmocom/gsm/gsm_utils.h>
-#include <osmocom/gsm/protocol/gsm_04_08.h>
-#include <osmocom/gsm/protocol/gsm_08_58.h>
 #include <osmocom/gsm/rsl.h>
 
 #include <osmocom/bb/common/l1ctl.h>
