4b742700a4457723652dd37c833a1b101583a7a3
Harald Welte <laforge@gnumonks.org>
2019-07-21 08:00:35 +0200

    virtphy: Fix array out of bounds access in getL1ctlPrimName()
    Closes: CID#198573
    Change-Id: I6504a7ffcf961d3162e6ab2ec8f5f2016ef12cde

--- src/host/virt_phy/src/logging.c
+++ src/host/virt_phy/src/logging.c
@@ -128,7 +128,7 @@ int ms_log_init(char *cat_mask)
 
 const char *getL1ctlPrimName(uint8_t type)
 {
-	if (type <= ARRAY_SIZE(l1ctlPrimNames))
+	if (type < ARRAY_SIZE(l1ctlPrimNames))
 		return l1ctlPrimNames[type];
 	else
 		return "Unknwon Primitive";
