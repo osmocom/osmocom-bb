e5a9077dbd10868b037f1f47d2ea8005d4bc0b67
Pau Espin Pedrol <pespin@sysmocom.de>
2020-11-25 18:45:26 +0100

    mobile: generate coredump and exit upon SIGABRT received
    Previous code relied on abort() switching sigaction to SIG_FDL +
    retriggering SIGABRT in case the signal handler returns, which would
    then generate the coredump + terminate the process.
    However, if a SIGABRT is received from somewhere else (kill -SIGABRT),
    then the process would print the talloc report and continue running,
    which is not desired.
    
    Change-Id: Ied0f47378a5d348b857424adb5c874c1c093b485
    Fixes: OS#4865

--- src/host/layer23/src/mobile/main.c
+++ src/host/layer23/src/mobile/main.c
@@ -188,9 +188,16 @@ void sighandler(int sigset)
 		osmo_signal_dispatch(SS_GLOBAL, S_GLOBAL_SHUTDOWN, &_quit);
 		break;
 	case SIGABRT:
-		/* in case of abort, we want to obtain a talloc report
-		 * and then return to the caller, who will abort the process
+		/* in case of abort, we want to obtain a talloc report and
+		 * then run default SIGABRT handler, who will generate coredump
+		 * and abort the process. abort() should do this for us after we
+		 * return, but program wouldn't exit if an external SIGABRT is
+		 * received.
 		 */
+		talloc_report_full(l23_ctx, stderr);
+		signal(SIGABRT, SIG_DFL);
+		raise(SIGABRT);
+		break;
 	case SIGUSR1:
 	case SIGUSR2:
 		talloc_report_full(l23_ctx, stderr);
