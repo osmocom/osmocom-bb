1741372556ecff5902fa0df6a0def6454caeea6a
Oliver Smith <osmith@sysmocom.de>
2021-12-14 12:24:54 +0100

    treewide: remove FSF address
    Remove the paragraph about writing to the Free Software Foundation's
    mailing address. The FSF has changed addresses in the past, and may do
    so again. In 2021 this is not useful, let's rather have a bit less
    boilerplate at the start of source files.
    
    Change-Id: I73be012c01c0108fb6951dbff91d50eb19b40c51

--- src/target/trx_toolkit/trx_sniff.py
+++ src/target/trx_toolkit/trx_sniff.py
@@ -17,10 +17,6 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 APP_CR_HOLDERS = [("2018-2020", "Vadim Yanitskiy <axilirator@gmail.com>")]
 
