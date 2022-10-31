731881712e8454b1b99f21092323e2022c3761af
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2022-10-31 20:39:32 +0700

    trxcon: fixup: trx_if_cmd_poweroff(): do not complain if trx->powered_up
    In commit [1] I introduced a regression by removing the conditional
    part completely.  My intention was to remove the logging statement,
    the return should be kept to prevent sending POWERON twice.
    
    This regression affects the following TTCN-3 testcases:
    
    * TC_sacch_chan_act_ho_async,
    * TC_sacch_chan_act_ho_sync.
    
    Change-Id: Iccd0bbac91a7899ef72df3dfe623b56eb66305fa
    Fixes: [1] 1c75c0012b621d6cc7194d70d79a8198eb7fddb1

--- src/host/trxcon/src/trx_if.c
+++ src/host/trxcon/src/trx_if.c
@@ -259,6 +259,8 @@ static int trx_if_cmd_poweroff(struct trx_instance *trx)
 
 static int trx_if_cmd_poweron(struct trx_instance *trx)
 {
+	if (trx->powered_up)
+		return -EAGAIN;
 	return trx_ctrl_cmd(trx, 1, "POWERON", "");
 }
 
