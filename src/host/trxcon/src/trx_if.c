osmocom/osmocom-bb

commit 178b92a1083b2348dafd066e16551cadcf90359d

trxcon: make burst pointer in trxcon_phyif_burst_ind const
The trx_data_rx_cb() needs to be modified because it's accessing and
modifying the receive buffer via the bi.burst pointer, which
becomes const after this patch.

Change-Id: I68773d247725a6dc2cbbc58b63c0fd19ffdb1a16
Related: OS#5599

src/host/trxcon/src/trx_if.c
@@ -631,6 +631,7 @@ static int trx_data_rx_cb(struct osmo_fd *ofd, unsigned int what)
 	struct trxcon_phyif_burst_ind bi;
 	uint8_t buf[TRXD_BUF_SIZE];
 	ssize_t read_len;
+	sbit_t *burst;
 
 	read_len = read(ofd->fd, buf, sizeof(buf));
 	if (read_len <= 0) {
@@ -644,22 +645,24 @@ static int trx_data_rx_cb(struct osmo_fd *ofd, unsigned int what)
 		return -EINVAL;
 	}
 
+	burst = (sbit_t *)&buf[8];
+
+	/* Convert ubits {254..0} to sbits {-127..127} in-place */
+	for (unsigned int i = 0; i < bi.burst_len; i++) {
+		if (buf[8 + i] == 255)
+			burst[i] = -127;
+		else
+			burst[i] = 127 - buf[8 + i];
+	}
+
 	bi = (struct trxcon_phyif_burst_ind) {
 		.tn = buf[0],
 		.fn = osmo_load32be(buf + 1),
 		.rssi = -(int8_t) buf[5],
 		.toa256 = (int16_t) (buf[6] << 8) | buf[7],
-		.burst = (sbit_t *)&buf[8],
+		.burst = burst,
 		.burst_len = 148,
 	};
-
-	/* Copy and convert bits {254..0} to sbits {-127..127} */
-	for (unsigned int i = 0; i < bi.burst_len; i++) {
-		if (buf[8 + i] == 255)
-			bi.burst[i] = -127;
-		else
-			bi.burst[i] = 127 - buf[8 + i];
-	}
 
 	if (bi.tn >= 8) {
 		LOGPFSMSL(trx->fi, DTRXD, LOGL_ERROR, "Illegal TS %d\n", bi.tn);

src/host/trxcon/include/osmocom/bb/trxcon/phyif.h
@@ -84,7 +84,7 @@ struct trxcon_phyif_burst_ind {
 	uint8_t tn;
 	int16_t toa256;
 	int8_t rssi;
-	sbit_t *burst;
+	const sbit_t *burst;
 	unsigned int burst_len;
 };
 

commit 7f1b99bc3b96e6f9d9547b1ec5b2ad2b0eb4c6b9

fixup: trxcon: make burst pointer in trxcon_phyif_burst_ind const
The for-loop was moved above the initialization of bi and kept using
bi.burst_len, which is indeed uninitialized.  Move it back.

Change-Id: I93a627233beef9157d47b35cafc42c53203619a7
Fixes: 178b92a1083b2348dafd066e16551cadcf90359d
Fixes: CID#300007

src/host/trxcon/src/trx_if.c
@@ -647,14 +647,6 @@ static int trx_data_rx_cb(struct osmo_fd *ofd, unsigned int what)
 
 	burst = (sbit_t *)&buf[8];
 
-	/* Convert ubits {254..0} to sbits {-127..127} in-place */
-	for (unsigned int i = 0; i < bi.burst_len; i++) {
-		if (buf[8 + i] == 255)
-			burst[i] = -127;
-		else
-			burst[i] = 127 - buf[8 + i];
-	}
-
 	bi = (struct trxcon_phyif_burst_ind) {
 		.tn = buf[0],
 		.fn = osmo_load32be(buf + 1),
@@ -663,6 +655,14 @@ static int trx_data_rx_cb(struct osmo_fd *ofd, unsigned int what)
 		.burst = burst,
 		.burst_len = 148,
 	};
+
+	/* Convert ubits {254..0} to sbits {-127..127} in-place */
+	for (unsigned int i = 0; i < bi.burst_len; i++) {
+		if (buf[8 + i] == 255)
+			burst[i] = -127;
+		else
+			burst[i] = 127 - buf[8 + i];
+	}
 
 	if (bi.tn >= 8) {
 		LOGPFSMSL(trx->fi, DTRXD, LOGL_ERROR, "Illegal TS %d\n", bi.tn);