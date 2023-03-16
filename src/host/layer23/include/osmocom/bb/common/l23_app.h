osmocom/osmocom-bb

commit 9feb5057da41611033a5881409c4fef2628d98a9

layer23: refactor the application API concept
With this set of changes we have a cleaner l23 app architecture:

* struct vty_app_info: all l23 applications must define this struct;
* struct vty_app_info: *cfg_supported() becomes a mask of L23_OPT_*;
* struct vty_app_info: explicitly set L23_OPT_* in all l23 apps;
* drop l23_app_info(), there can be only one vty_app_info per an app;

It's no more needed to obtain the vty_app_info by calling a function
and checking the returned value against NULL everywhere.  This kind
of information is rather static (not dynamically composed) and needs
not to be encapsulated into functions.

Change-Id: I89004cd5308927305f79b102f7b695709148df6d

src/host/layer23/include/osmocom/bb/common/l23_app.h
@@ -60,10 +60,10 @@ extern int (*l23_app_exit)(void);
 struct l23_app_info {
 	const char *copyright;
 	const char *contribution;
-	struct vty_app_info *vty_info; /* L23_OPT_VTY */
+	const struct vty_app_info *vty_info; /* L23_OPT_VTY */
 
 	char *getopt_string;
-	int (*cfg_supported)();
+	uint32_t opt_supported; /* mask of L23_OPT_* */
 	int (*cfg_print_help)();
 	int (*cfg_getopt_opt)(struct option **options);
 	int (*cfg_handle_opt)(int c,const char *optarg);
@@ -71,6 +71,7 @@ struct l23_app_info {
 	osmo_tundev_data_ind_cb_t tun_data_ind_cb;
 };
 
-extern struct l23_app_info *l23_app_info();
+/* all l23 apps must define this structure */
+extern const struct l23_app_info l23_app_info;
 
 #endif /* _L23_APP_H */

src/host/layer23/src/common/main.c
@@ -71,40 +71,33 @@ static void print_usage(const char *app)
 
 static void print_help(void)
 {
-	int options = 0xff;
-	struct l23_app_info *app = l23_app_info();
-
-	if (app && app->cfg_supported != 0)
-		options = app->cfg_supported();
-
 	printf(" Some help...\n");
 	printf("  -h --help		this text\n");
 	printf("  -s --socket		/tmp/osmocom_l2. Path to the unix "
 		"domain socket (l2)\n");
 
-	if (options & L23_OPT_SAP)
+	if (l23_app_info.opt_supported & L23_OPT_SAP)
 		printf("  -S --sap		/tmp/osmocom_sap. Path to the "
 			"unix domain socket (BTSAP)\n");
 
-	if (options & L23_OPT_ARFCN)
+	if (l23_app_info.opt_supported & L23_OPT_ARFCN)
 		printf("  -a --arfcn NR		The ARFCN to be used for layer2.\n");
 
-	if (options & L23_OPT_TAP)
+	if (l23_app_info.opt_supported & L23_OPT_TAP)
 		printf("  -i --gsmtap-ip	The destination IP used for GSMTAP.\n");
 
-	if (options & L23_OPT_VTY)
+	if (l23_app_info.opt_supported & L23_OPT_VTY)
 		printf("  -c --config-file	The path to the VTY configuration file.\n");
 
-	if (options & L23_OPT_DBG)
+	if (l23_app_info.opt_supported & L23_OPT_DBG)
 		printf("  -d --debug		Change debug flags.\n");
 
-	if (app && app->cfg_print_help)
-		app->cfg_print_help();
+	if (l23_app_info.cfg_print_help != NULL)
+		l23_app_info.cfg_print_help();
 }
 
 static void build_config(char **opt, struct option **option)
 {
-	struct l23_app_info *app;
 	struct option *app_opp = NULL;
 	int app_len = 0, len;
 
@@ -119,13 +112,12 @@ static void build_config(char **opt, struct option **option)
 	};
 
 
-	app = l23_app_info();
 	*opt = talloc_asprintf(l23_ctx, "hs:S:a:i:c:d:%s",
-			       app && app->getopt_string ? app->getopt_string : "");
+			       l23_app_info.getopt_string ? l23_app_info.getopt_string : "");
 
 	len = ARRAY_SIZE(long_options);
-	if (app && app->cfg_getopt_opt)
-		app_len = app->cfg_getopt_opt(&app_opp);
+	if (l23_app_info.cfg_getopt_opt != NULL)
+		app_len = l23_app_info.cfg_getopt_opt(&app_opp);
 
 	*option = talloc_zero_array(l23_ctx, struct option, len + app_len + 1);
 	memcpy(*option, long_options, sizeof(long_options));
@@ -133,7 +125,7 @@ static void build_config(char **opt, struct option **option)
 		memcpy(*option + len, app_opp, app_len * sizeof(struct option));
 }
 
-static void handle_options(int argc, char **argv, struct l23_app_info *app)
+static void handle_options(int argc, char **argv)
 {
 	struct option *long_options;
 	char *opt;
@@ -173,8 +165,8 @@ static void handle_options(int argc, char **argv, struct l23_app_info *app)
 			log_parse_category_mask(osmo_stderr_target, optarg);
 			break;
 		default:
-			if (app && app->cfg_handle_opt)
-				app->cfg_handle_opt(c, optarg);
+			if (l23_app_info.cfg_handle_opt != NULL)
+				l23_app_info.cfg_handle_opt(c, optarg);
 			break;
 		}
 	}
@@ -200,32 +192,29 @@ void sighandler(int sigset)
 
 static void print_copyright(void)
 {
-	struct l23_app_info *app;
-	app = l23_app_info();
-
 	printf("%s"
 	       "%s\n"
 	       "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>\n"
 	       "This is free software: you are free to change and redistribute it.\n"
 	       "There is NO WARRANTY, to the extent permitted by law.\n\n",
-	       app && app->copyright ? app->copyright : "",
-	       app && app->contribution ? app->contribution : "");
+	       l23_app_info.copyright ? l23_app_info.copyright : "",
+	       l23_app_info.contribution ? l23_app_info.contribution : "");
 }
 
-static int _vty_init(struct l23_app_info *app)
+static int _vty_init(void)
 {
-	static struct vty_app_info l23_vty_info = {
-		.name = "OsmocomBB",
-		.version = PACKAGE_VERSION,
-	};
+	struct vty_app_info info;
 	int rc;
 
-	OSMO_ASSERT(app->vty_info);
-	app->vty_info->tall_ctx = l23_ctx;
-	vty_init(app->vty_info ? : &l23_vty_info);
+	OSMO_ASSERT(l23_app_info.vty_info != NULL);
+	info = *l23_app_info.vty_info;
+	info.tall_ctx = l23_ctx;
+
+	vty_init(&info);
 	logging_vty_add_cmds();
-	if (app->vty_init)
-		app->vty_init();
+
+	if (l23_app_info.vty_init != NULL)
+		l23_app_info.vty_init();
 	if (config_file) {
 		LOGP(DLGLOBAL, LOGL_INFO, "Using configuration from '%s'\n", config_file);
 		l23_vty_reading = true;
@@ -249,8 +238,6 @@ static int _vty_init(struct l23_app_info *app)
 int main(int argc, char **argv)
 {
 	int rc;
-	struct l23_app_info *app;
-	unsigned int app_supp_opt = 0x00;
 
 	INIT_LLIST_HEAD(&ms_list);
 
@@ -273,18 +260,14 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	app = l23_app_info();
-	if (app && app->cfg_supported != NULL)
-		app_supp_opt = app->cfg_supported();
+	handle_options(argc, argv);
 
-	handle_options(argc, argv, app);
-
-	if (app_supp_opt & L23_OPT_VTY) {
-		if (_vty_init(app) < 0)
+	if (l23_app_info.opt_supported & L23_OPT_VTY) {
+		if (_vty_init() < 0)
 			exit(1);
 	}
 
-	if (app_supp_opt & L23_OPT_TAP) {
+	if (l23_app_info.opt_supported & L23_OPT_TAP) {
 		if (gsmtap_ip) {
 			if (l23_cfg.gsmtap.remote_host != NULL) {
 				LOGP(DLGLOBAL, LOGL_NOTICE,

src/host/layer23/src/mobile/app_mobile.c
@@ -490,29 +490,18 @@ static int _mobile_vty_init(void)
 	return ms_vty_init();
 }
 
-static int l23_cfg_supported(void)
-{
-	return L23_OPT_TAP | L23_OPT_VTY | L23_OPT_DBG;
-}
-
-static struct vty_app_info _mobile_vty_info = {
+static const struct vty_app_info _mobile_vty_info = {
 	.name = "mobile",
 	.version = PACKAGE_VERSION,
 };
 
-static struct l23_app_info info = {
+const struct l23_app_info l23_app_info = {
 	.copyright = "Copyright (C) 2010-2015 Andreas Eversberg, Sylvain Munaut, Holger Freyther, Harald Welte\n",
 	.contribution = "Contributions by Alex Badea, Pablo Neira, Steve Markgraf and others\n",
-	.cfg_supported = &l23_cfg_supported,
+	.opt_supported = L23_OPT_TAP | L23_OPT_VTY | L23_OPT_DBG,
 	.vty_info = &_mobile_vty_info,
 	.vty_init = _mobile_vty_init,
 };
-
-struct l23_app_info *l23_app_info(void)
-{
-	return &info;
-}
-
 
 void mobile_set_started(struct osmocom_ms *ms, bool state)
 {

src/host/layer23/src/mobile/main.c
@@ -206,32 +206,29 @@ void sighandler(int sigset)
 
 static void print_copyright(void)
 {
-	struct l23_app_info *app;
-	app = l23_app_info();
-
 	printf("%s"
 	       "%s\n"
 	       "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>\n"
 	       "This is free software: you are free to change and redistribute it.\n"
 	       "There is NO WARRANTY, to the extent permitted by law.\n\n",
-	       app && app->copyright ? app->copyright : "",
-	       app && app->contribution ? app->contribution : "");
+	       l23_app_info.copyright ? l23_app_info.copyright : "",
+	       l23_app_info.contribution ? l23_app_info.contribution : "");
 }
 
-static int _vty_init(struct l23_app_info *app)
+static int _vty_init(void)
 {
-	static struct vty_app_info l23_vty_info = {
-		.name = "OsmocomBB",
-		.version = PACKAGE_VERSION,
-	};
+	struct vty_app_info info;
 	int rc;
 
-	OSMO_ASSERT(app->vty_info);
-	app->vty_info->tall_ctx = l23_ctx;
-	vty_init(app->vty_info ? : &l23_vty_info);
+	OSMO_ASSERT(l23_app_info.vty_info != NULL);
+	info = *l23_app_info.vty_info;
+	info.tall_ctx = l23_ctx;
+
+	vty_init(&info);
 	logging_vty_add_cmds();
-	if (app->vty_init)
-		app->vty_init();
+
+	if (l23_app_info.vty_init != NULL)
+		l23_app_info.vty_init();
 	if (config_file) {
 		LOGP(DLGLOBAL, LOGL_INFO, "Using configuration from '%s'\n", config_file);
 		l23_vty_reading = true;
@@ -255,8 +252,6 @@ static int _vty_init(struct l23_app_info *app)
 int main(int argc, char **argv)
 {
 	int rc;
-	struct l23_app_info *app;
-	unsigned int app_supp_opt = 0x00;
 
 	print_copyright();
 
@@ -276,11 +271,6 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Failed during l23_app_init()\n");
 		exit(1);
 	}
-
-	app = l23_app_info();
-	if (app && app->cfg_supported != NULL)
-		app_supp_opt = app->cfg_supported();
-
 
 	rc = handle_options(argc, argv);
 	if (rc) { /* Abort in case of parsing errors */
@@ -306,8 +296,8 @@ int main(int argc, char **argv)
 	config_dir = talloc_strdup(l23_ctx, config_file);
 	config_dir = dirname(config_dir);
 
-	if (app_supp_opt & L23_OPT_VTY) {
-		if (_vty_init(app) < 0)
+	if (l23_app_info.opt_supported & L23_OPT_VTY) {
+		if (_vty_init() < 0)
 			exit(1);
 	}
 

src/host/layer23/src/modem/app_modem.c
@@ -572,26 +572,16 @@ int l23_app_init(void)
 	return 0;
 }
 
-static int l23_cfg_supported(void)
-{
-	return L23_OPT_ARFCN | L23_OPT_TAP | L23_OPT_VTY | L23_OPT_DBG;
-}
-
-static struct vty_app_info _modem_vty_info = {
+static const struct vty_app_info _modem_vty_info = {
 	.name = "modem",
 	.version = PACKAGE_VERSION,
 	.go_parent_cb = modem_vty_go_parent,
 };
 
-static struct l23_app_info info = {
+const struct l23_app_info l23_app_info = {
 	.copyright = "Copyright (C) 2022 by sysmocom - s.m.f.c. GmbH <info@sysmocom.de>\n",
-	.cfg_supported = &l23_cfg_supported,
+	.opt_supported = L23_OPT_ARFCN | L23_OPT_TAP | L23_OPT_VTY | L23_OPT_DBG,
 	.vty_info = &_modem_vty_info,
 	.vty_init = modem_vty_init,
 	.tun_data_ind_cb = modem_tun_data_ind_cb,
 };
-
-struct l23_app_info *l23_app_info(void)
-{
-	return &info;
-}

src/host/layer23/src/common/apn.c
@@ -61,7 +61,6 @@ void apn_free(struct osmobb_apn *apn)
 
 int apn_start(struct osmobb_apn *apn)
 {
-	struct l23_app_info *app_info = l23_app_info();
 	int rc;
 
 	if (apn->started)
@@ -69,8 +68,8 @@ int apn_start(struct osmobb_apn *apn)
 
 	LOGPAPN(LOGL_INFO, apn, "Opening TUN device %s\n", apn->cfg.dev_name);
 	/* Set TUN library callback. Must have been configured by the app: */
-	OSMO_ASSERT(app_info && app_info->tun_data_ind_cb);
-	osmo_tundev_set_data_ind_cb(apn->tun, app_info->tun_data_ind_cb);
+	OSMO_ASSERT(l23_app_info.tun_data_ind_cb);
+	osmo_tundev_set_data_ind_cb(apn->tun, l23_app_info.tun_data_ind_cb);
 	osmo_tundev_set_dev_name(apn->tun, apn->cfg.dev_name);
 	osmo_tundev_set_netns_name(apn->tun, apn->cfg.dev_netns_name);
 

src/host/layer23/src/common/vty.c
@@ -559,18 +559,12 @@ static void l23_vty_init_gsmtap(void)
 
 int l23_vty_init(int (*config_write_ms_node_cb)(struct vty *), osmo_signal_cbfn *l23_vty_signal_cb)
 {
-	struct l23_app_info *app;
-	unsigned int app_supp_opt = 0x00;
 	int rc = 0;
 
-	app = l23_app_info();
-	if (app && app->cfg_supported != NULL)
-		app_supp_opt = app->cfg_supported();
-
-	if (app_supp_opt & L23_OPT_TAP)
+	if (l23_app_info.opt_supported & L23_OPT_TAP)
 		l23_vty_init_gsmtap();
 
-	if (app_supp_opt & L23_OPT_VTY)
+	if (l23_app_info.opt_supported & L23_OPT_VTY)
 		osmo_stats_vty_add_cmds();
 
 	install_node(&ms_node, config_write_ms_node_cb);

src/host/layer23/src/misc/app_bcch_scan.c
@@ -75,12 +75,8 @@ int l23_app_init(void)
 	return osmo_signal_register_handler(SS_L1CTL, &signal_cb, NULL);
 }
 
-static struct l23_app_info info = {
+const struct l23_app_info l23_app_info = {
 	.copyright	= "Copyright (C) 2010 Harald Welte <laforge@gnumonks.org>\n",
 	.contribution	= "Contributions by Holger Hans Peter Freyther\n",
+	.opt_supported = L23_OPT_ARFCN | L23_OPT_TAP | L23_OPT_DBG,
 };
-
-struct l23_app_info *l23_app_info(void)
-{
-	return &info;
-}

commit b2dfbb88591395135a494c048cac5c93865720de

layer23/{mobile,modem}: fix segfault on VTY connection
It was a mistake to call vty_init(), passing it a pointer to the
vty_app_info structure allocated on the stack, because it gets
overwritten when the calling function _vty_init() returns.

Change-Id: I75843a964254243c70bedcf8ff97d854107ee21a
Fixes: 9feb5057 "layer23: refactor the application API concept"

src/host/layer23/include/osmocom/bb/common/l23_app.h
@@ -60,7 +60,7 @@ extern int (*l23_app_exit)(void);
 struct l23_app_info {
 	const char *copyright;
 	const char *contribution;
-	const struct vty_app_info *vty_info; /* L23_OPT_VTY */
+	struct vty_app_info *vty_info; /* L23_OPT_VTY */
 
 	char *getopt_string;
 	uint32_t opt_supported; /* mask of L23_OPT_* */

src/host/layer23/src/common/main.c
@@ -203,14 +203,12 @@ static void print_copyright(void)
 
 static int _vty_init(void)
 {
-	struct vty_app_info info;
 	int rc;
 
 	OSMO_ASSERT(l23_app_info.vty_info != NULL);
-	info = *l23_app_info.vty_info;
-	info.tall_ctx = l23_ctx;
+	l23_app_info.vty_info->tall_ctx = l23_ctx;
 
-	vty_init(&info);
+	vty_init(l23_app_info.vty_info);
 	logging_vty_add_cmds();
 
 	if (l23_app_info.vty_init != NULL)

src/host/layer23/src/mobile/app_mobile.c
@@ -490,7 +490,7 @@ static int _mobile_vty_init(void)
 	return ms_vty_init();
 }
 
-static const struct vty_app_info _mobile_vty_info = {
+static struct vty_app_info _mobile_vty_info = {
 	.name = "mobile",
 	.version = PACKAGE_VERSION,
 };

src/host/layer23/src/mobile/main.c
@@ -217,14 +217,12 @@ static void print_copyright(void)
 
 static int _vty_init(void)
 {
-	struct vty_app_info info;
 	int rc;
 
 	OSMO_ASSERT(l23_app_info.vty_info != NULL);
-	info = *l23_app_info.vty_info;
-	info.tall_ctx = l23_ctx;
+	l23_app_info.vty_info->tall_ctx = l23_ctx;
 
-	vty_init(&info);
+	vty_init(l23_app_info.vty_info);
 	logging_vty_add_cmds();
 
 	if (l23_app_info.vty_init != NULL)

src/host/layer23/src/modem/app_modem.c
@@ -185,7 +185,7 @@ int l23_app_init(void)
 	return 0;
 }
 
-static const struct vty_app_info _modem_vty_info = {
+static struct vty_app_info _modem_vty_info = {
 	.name = "modem",
 	.version = PACKAGE_VERSION,
 	.go_parent_cb = modem_vty_go_parent,