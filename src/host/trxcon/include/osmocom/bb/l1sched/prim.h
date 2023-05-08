osmocom/osmocom-bb

commit ff9db9def78d9c2439c8ff3196746bf6df987886

trxcon/l1sched: rework the primitive API
The goal is to simplify primitive management, and allow passing data
between different components without having to re-allocate memory and
copy it over several times.  This patch has been tested by running
ttcn3-bts-test, no regressions observed.

* Use msgb and prim API from libosmocore,
* Move l1sched_prim definitions to its own header file,
* Move Tx queue from per-timeslot to per-lchan state,
* Route prims via l1sched_prim_{to,from}_user() functions,
* Remove GSMTAP stuff from sched_lchan_desc[].

Change-Id: I73576bd0ea10a5663ba6254283812c275cc3fa46
Related: OS#5500

src/host/trxcon/include/osmocom/bb/l1sched/prim.h
@@ -1,0 +1,130 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <osmocom/core/msgb.h>
+#include <osmocom/core/prim.h>
+#include <osmocom/core/utils.h>
+
+#define l1sched_prim_from_msgb(msg) \
+	((struct l1sched_prim *)(msg)->l1h)
+
+#define l1sched_prim_data_from_msgb(msg) \
+	((uint8_t *)msgb_l2(msg))
+
+#define l1sched_prim_type_from_msgb(msg) \
+	l1sched_prim_from_msgb(msg)->oph.primitive
+
+#define L1SCHED_PRIM_STR_FMT "%s.%s"
+#define L1SCHED_PRIM_STR_ARGS(prim) \
+	l1sched_prim_type_name((prim)->oph.primitive), \
+	osmo_prim_operation_name((prim)->oph.operation)
+
+enum l1sched_prim_type {
+	L1SCHED_PRIM_T_DATA,			/* Req | Ind | Cnf */
+	L1SCHED_PRIM_T_RACH,			/* Req | Cnf */
+	L1SCHED_PRIM_T_SCH,			/* Ind */
+	L1SCHED_PRIM_T_PCHAN_COMB,		/* Ind */
+};
+
+extern const struct value_string l1sched_prim_type_names[];
+static inline const char *l1sched_prim_type_name(enum l1sched_prim_type val)
+{
+	return get_value_string(l1sched_prim_type_names, val);
+}
+
+/*! Common header for L1SCHED_PRIM_T_{DATA,RACH} */
+struct l1sched_prim_chdr {
+	/*! TDMA Frame Number */
+	uint32_t frame_nr;
+	/*! RSL Channel Number */
+	uint8_t chan_nr;
+	/*! RSL Link Identifier */
+	uint8_t link_id;
+	/*! Traffic or signalling */
+	bool traffic;
+};
+
+/*! Payload of L1SCHED_PRIM_T_DATA | Ind */
+struct l1sched_prim_data_ind {
+	/*! Common sub-header */
+	struct l1sched_prim_chdr chdr;
+	int16_t toa256;
+	int8_t rssi;
+	int n_errors;
+	int n_bits_total;
+};
+
+/*! Payload of L1SCHED_PRIM_T_RACH | {Req,Cnf} */
+struct l1sched_prim_rach {
+	/*! Common sub-header */
+	struct l1sched_prim_chdr chdr;
+	/*! Training Sequence (only for 11-bit RA) */
+	uint8_t synch_seq;
+	/*! Transmission offset (how many frames to skip) */
+	uint8_t offset;
+	/*! RA value is 11 bit */
+	bool is_11bit;
+	/*! RA value */
+	uint16_t ra;
+};
+
+struct l1sched_prim {
+	/*! Primitive header */
+	struct osmo_prim_hdr oph;
+	/*! Type specific header */
+	union {
+		/*! L1SCHED_PRIM_T_DATA | Req */
+		struct l1sched_prim_chdr data_req;
+		/*! L1SCHED_PRIM_T_DATA | Cnf */
+		struct l1sched_prim_chdr data_cnf;
+		/*! L1SCHED_PRIM_T_DATA | Ind */
+		struct l1sched_prim_data_ind data_ind;
+
+		/*! L1SCHED_PRIM_T_RACH | Req */
+		struct l1sched_prim_rach rach_req;
+		/*! L1SCHED_PRIM_T_RACH | Cnf */
+		struct l1sched_prim_rach rach_cnf;
+
+		/*! L1SCHED_PRIM_T_SCH | Ind */
+		struct {
+			/*! TDMA frame number */
+			uint32_t frame_nr;
+			/*! BSIC */
+			uint8_t bsic;
+		} sch_ind;
+
+		/*! L1SCHED_PRIM_T_PCHAN_COMB | Ind */
+		struct {
+			/*! Timeslot number */
+			uint8_t tn;
+			/*! Channel combination for a timeslot */
+			enum gsm_phys_chan_config pchan;
+		} pchan_comb_ind;
+	};
+};
+
+
+struct l1sched_state;
+struct l1sched_lchan_state;
+
+void l1sched_prim_init(struct msgb *msg,
+		       enum l1sched_prim_type type,
+		       enum osmo_prim_operation op);
+
+struct msgb *l1sched_prim_alloc(enum l1sched_prim_type type,
+				enum osmo_prim_operation op,
+				size_t extra_size);
+
+struct msgb *l1sched_lchan_prim_dequeue(struct l1sched_lchan_state *lchan, uint32_t fn);
+void l1sched_lchan_prim_assign_dummy(struct l1sched_lchan_state *lchan);
+void l1sched_lchan_prim_drop(struct l1sched_lchan_state *lchan);
+
+int l1sched_lchan_emit_data_ind(struct l1sched_lchan_state *lchan,
+				const uint8_t *data, size_t data_len,
+				int n_errors, int n_bits_total, bool traffic);
+int l1sched_lchan_emit_data_cnf(struct l1sched_lchan_state *lchan, uint32_t fn);
+
+int l1sched_prim_from_user(struct l1sched_state *sched, struct msgb *msg);
+int l1sched_prim_to_user(struct l1sched_state *sched, struct msgb *msg);

src/host/trxcon/src/sched_lchan_sch.c
@@ -61,6 +61,21 @@ static void decode_sb(struct gsm_time *time, uint8_t *bsic, uint8_t *sb_info)
 	time->fn = gsm_gsmtime2fn(time);
 }
 
+static int handle_sch_ind(struct l1sched_state *sched, uint32_t fn, uint8_t bsic)
+{
+	struct l1sched_prim *prim;
+	struct msgb *msg;
+
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_SCH, PRIM_OP_INDICATION, 0);
+	OSMO_ASSERT(msg != NULL);
+
+	prim = l1sched_prim_from_msgb(msg);
+	prim->sch_ind.frame_nr = fn;
+	prim->sch_ind.bsic = bsic;
+
+	return l1sched_prim_to_user(sched, msg);
+}
+
 int rx_sch_fn(struct l1sched_lchan_state *lchan,
 	      const struct l1sched_burst_ind *bi)
 {
@@ -100,8 +115,5 @@ int rx_sch_fn(struct l1sched_lchan_state *lchan,
 	/* Update BSIC value in the scheduler state */
 	lchan->ts->sched->bsic = bsic;
 
-	l1sched_handle_data_ind(lchan, (const uint8_t *)&time, sizeof(time),
-				0, 39 * 2, L1SCHED_DT_OTHER);
-
-	return 0;
+	return handle_sch_ind(lchan->ts->sched, time.fn, bsic);
 }

src/host/trxcon/src/sched_trx.c
@@ -68,18 +68,20 @@ static const uint8_t meas_rep_dummy[] = {
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-static int l1sched_cfg_pchan_comb_req(struct l1sched_state *sched,
+static int l1sched_cfg_pchan_comb_ind(struct l1sched_state *sched,
 				      uint8_t tn, enum gsm_phys_chan_config pchan)
 {
-	const struct l1sched_config_req cr = {
-		.type = L1SCHED_CFG_PCHAN_COMB,
-		.pchan_comb = {
-			.tn = tn,
-			.pchan = pchan,
-		},
-	};
+	struct l1sched_prim *prim;
+	struct msgb *msg;
 
-	return l1sched_handle_config_req(sched, &cr);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_PCHAN_COMB, PRIM_OP_INDICATION, 0);
+	OSMO_ASSERT(msg != NULL);
+
+	prim = l1sched_prim_from_msgb(msg);
+	prim->pchan_comb_ind.tn = tn;
+	prim->pchan_comb_ind.pchan = pchan;
+
+	return l1sched_prim_to_user(sched, msg);
 }
 
 static void l1sched_a5_burst_enc(struct l1sched_lchan_state *lchan,
@@ -121,11 +123,11 @@ void l1sched_pull_burst(struct l1sched_state *sched, struct l1sched_burst_req *b
 
 	/* If no primitive is being processed, try obtaining one from Tx queue */
 	if (lchan->prim == NULL)
-		lchan->prim = l1sched_prim_dequeue(&ts->tx_prims, br->fn, lchan);
+		lchan->prim = l1sched_lchan_prim_dequeue(lchan, br->fn);
 	if (lchan->prim == NULL) {
 		/* If CBTX (Continuous Burst Transmission) is required */
 		if (l1sched_lchan_desc[chan].flags & L1SCHED_CH_FLAG_CBTX)
-			l1sched_prim_dummy(lchan);
+			l1sched_lchan_prim_assign_dummy(lchan);
 		if (lchan->prim == NULL)
 			return;
 	}
@@ -134,7 +136,8 @@ void l1sched_pull_burst(struct l1sched_state *sched, struct l1sched_burst_req *b
 
 	/* Handover RACH needs to be handled regardless of the
 	 * current channel type and the associated handler. */
-	if (L1SCHED_PRIM_IS_RACH(lchan->prim) && lchan->prim->chan != L1SCHED_RACH)
+	if (l1sched_prim_type_from_msgb(lchan->prim) == L1SCHED_PRIM_T_RACH &&
+	    lchan->type != L1SCHED_RACH)
 		handler = l1sched_lchan_desc[L1SCHED_RACH].tx_fn;
 
 	/* Poke lchan handler */
@@ -272,15 +275,12 @@ void l1sched_del_ts(struct l1sched_state *sched, int tn)
 		talloc_free(lchan);
 	}
 
-	/* Flush queue primitives for TX */
-	l1sched_prim_flush_queue(&ts->tx_prims);
-
 	/* Remove ts from list and free memory */
 	sched->ts[tn] = NULL;
 	talloc_free(ts);
 
 	/* Notify transceiver about that */
-	l1sched_cfg_pchan_comb_req(sched, tn, GSM_PCHAN_NONE);
+	l1sched_cfg_pchan_comb_ind(sched, tn, GSM_PCHAN_NONE);
 }
 
 #define LAYOUT_HAS_LCHAN(layout, lchan) \
@@ -316,8 +316,6 @@ int l1sched_configure_ts(struct l1sched_state *sched, int tn,
 		    "(Re)configure TDMA timeslot #%u as %s\n",
 		    tn, ts->mf_layout->name);
 
-	/* Init queue primitives for TX */
-	INIT_LLIST_HEAD(&ts->tx_prims);
 	/* Init logical channels list */
 	INIT_LLIST_HEAD(&ts->lchans);
 
@@ -337,6 +335,9 @@ int l1sched_configure_ts(struct l1sched_state *sched, int tn,
 		/* Set channel type */
 		lchan->type = type;
 
+		/* Init the Tx queue */
+		INIT_LLIST_HEAD(&lchan->tx_prims);
+
 		/* Add to the list of channel states */
 		llist_add_tail(&lchan->list, &ts->lchans);
 
@@ -346,7 +347,7 @@ int l1sched_configure_ts(struct l1sched_state *sched, int tn,
 	}
 
 	/* Notify transceiver about TS activation */
-	l1sched_cfg_pchan_comb_req(sched, tn, config);
+	l1sched_cfg_pchan_comb_ind(sched, tn, config);
 
 	return 0;
 }
@@ -364,9 +365,6 @@ int l1sched_reset_ts(struct l1sched_state *sched, int tn)
 	/* Undefine multiframe layout */
 	ts->mf_layout = NULL;
 
-	/* Flush queue primitives for TX */
-	l1sched_prim_flush_queue(&ts->tx_prims);
-
 	/* Deactivate all logical channels */
 	l1sched_deactivate_all_lchans(ts);
 
@@ -377,7 +375,7 @@ int l1sched_reset_ts(struct l1sched_state *sched, int tn)
 	}
 
 	/* Notify transceiver about that */
-	l1sched_cfg_pchan_comb_req(sched, tn, GSM_PCHAN_NONE);
+	l1sched_cfg_pchan_comb_ind(sched, tn, GSM_PCHAN_NONE);
 
 	return 0;
 }
@@ -541,6 +539,8 @@ int l1sched_activate_lchan(struct l1sched_ts *ts, enum l1sched_lchan_type chan)
 
 static void l1sched_reset_lchan(struct l1sched_lchan_state *lchan)
 {
+	struct msgb *msg;
+
 	/* Prevent NULL-pointer deference */
 	OSMO_ASSERT(lchan != NULL);
 
@@ -567,6 +567,10 @@ static void l1sched_reset_lchan(struct l1sched_lchan_state *lchan)
 
 	/* Forget the current prim */
 	l1sched_lchan_prim_drop(lchan);
+
+	/* Flush the queue of pending Tx prims */
+	while ((msg = msgb_dequeue(&lchan->tx_prims)) != NULL)
+		msgb_free(msg);
 
 	/* Channel specific stuff */
 	if (L1SCHED_CHAN_IS_TCH(lchan->type)) {
@@ -655,20 +659,6 @@ enum gsm_phys_chan_config l1sched_chan_nr2pchan_config(uint8_t chan_nr)
 		return GSM_PCHAN_PDCH;
 
 	return GSM_PCHAN_NONE;
-}
-
-enum l1sched_lchan_type l1sched_chan_nr2lchan_type(uint8_t chan_nr,
-	uint8_t link_id)
-{
-	int i;
-
-	/* Iterate over all known lchan types */
-	for (i = 0; i < _L1SCHED_CHAN_MAX; i++)
-		if (l1sched_lchan_desc[i].chan_nr == (chan_nr & RSL_CHAN_NR_MASK))
-			if (l1sched_lchan_desc[i].link_id == link_id)
-				return i;
-
-	return L1SCHED_IDLE;
 }
 
 static void l1sched_a5_burst_dec(struct l1sched_lchan_state *lchan,

src/host/trxcon/src/trxcon_fsm.c
@@ -283,21 +283,25 @@ static void handle_tx_access_burst_req(struct osmo_fsm_inst *fi,
 				       const struct trxcon_param_tx_access_burst_req *req)
 {
 	struct trxcon_inst *trxcon = fi->priv;
-	enum l1sched_ts_prim_type prim_type;
-	const struct l1sched_ts_prim *prim;
+	struct l1sched_prim *prim;
+	struct msgb *msg;
 
-	const struct l1sched_ts_prim_rach rach = {
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_RACH, PRIM_OP_REQUEST, 0);
+	OSMO_ASSERT(msg != NULL);
+
+	prim = l1sched_prim_from_msgb(msg);
+	prim->rach_req = (struct l1sched_prim_rach) {
+		.chdr = {
+			.chan_nr = req->chan_nr,
+			.link_id = req->link_id,
+		},
 		.synch_seq = req->synch_seq,
 		.offset = req->offset,
+		.is_11bit = req->is_11bit,
 		.ra = req->ra,
 	};
 
-	prim_type = req->is_11bit ? L1SCHED_PRIM_RACH11 : L1SCHED_PRIM_RACH8;
-	prim = l1sched_prim_push(trxcon->sched, prim_type,
-				 req->chan_nr, req->link_id,
-				 (const uint8_t *)&rach, sizeof(rach));
-	if (prim == NULL)
-		LOGPFSML(fi, LOGL_ERROR, "Failed to enqueue a prim\n");
+	l1sched_prim_from_user(trxcon->sched, msg);
 }
 
 static void handle_dch_est_req(struct osmo_fsm_inst *fi,
@@ -485,15 +489,21 @@ static void trxcon_st_dedicated_action(struct osmo_fsm_inst *fi,
 	case TRXCON_EV_TX_DATA_REQ:
 	{
 		const struct trxcon_param_tx_data_req *req = data;
-		struct l1sched_ts_prim *prim;
+		struct l1sched_prim *prim;
+		struct msgb *msg;
 
-		prim = l1sched_prim_push(trxcon->sched, L1SCHED_PRIM_DATA,
-					 req->chan_nr, req->link_id,
-					 req->data, req->data_len);
-		if (prim == NULL) {
-			LOGPFSML(fi, LOGL_ERROR, "Failed to enqueue a prim\n");
-			return;
-		}
+		msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST, req->data_len);
+		OSMO_ASSERT(msg != NULL);
+
+		prim = l1sched_prim_from_msgb(msg);
+		prim->data_req = (struct l1sched_prim_chdr) {
+			.chan_nr = req->chan_nr,
+			.link_id = req->link_id,
+			.traffic = req->traffic,
+		};
+
+		memcpy(msgb_put(msg, req->data_len), req->data, req->data_len);
+		l1sched_prim_from_user(trxcon->sched, msg);
 		break;
 	}
 	case TRXCON_EV_TX_DATA_CNF:
@@ -547,21 +557,24 @@ static void trxcon_st_packet_data_action(struct osmo_fsm_inst *fi,
 	case TRXCON_EV_GPRS_UL_BLOCK_REQ:
 	{
 		struct l1gprs_prim_ul_block_req block_req;
-		const struct msgb *msg = data;
-		struct l1sched_ts_prim *prim;
+		struct l1sched_prim *prim;
+		struct msgb *msg = data;
 
 		if (l1gprs_handle_ul_block_req(trxcon->gprs, &block_req, msg) != 0)
 			return;
 
-		prim = l1sched_prim_push(trxcon->sched, L1SCHED_PRIM_DATA,
-					 RSL_CHAN_OSMO_PDCH | block_req.hdr.tn, 0x00,
-					 block_req.data, block_req.data_len);
-		if (prim == NULL) {
-			LOGPFSML(fi, LOGL_ERROR, "Failed to enqueue a prim\n");
-			return;
-		}
+		msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST, block_req.data_len);
+		OSMO_ASSERT(msg != NULL);
 
-		prim->fn = block_req.hdr.fn;
+		prim = l1sched_prim_from_msgb(msg);
+		prim->data_req = (struct l1sched_prim_chdr) {
+			.frame_nr = block_req.hdr.fn,
+			.chan_nr = RSL_CHAN_OSMO_PDCH | block_req.hdr.tn,
+			.link_id = 0x00,
+		};
+
+		memcpy(msgb_put(msg, block_req.data_len), block_req.data, block_req.data_len);
+		l1sched_prim_from_user(trxcon->sched, msg);
 		break;
 	}
 	case TRXCON_EV_RX_DATA_IND:

src/host/trxcon/include/osmocom/bb/l1sched/Makefile.am
@@ -1,4 +1,5 @@
 noinst_HEADERS = \
 	l1sched.h \
 	logging.h \
+	prim.h \
 	$(NULL)

src/host/trxcon/include/osmocom/bb/l1sched/l1sched.h
@@ -14,6 +14,8 @@
 #include <osmocom/core/linuxlist.h>
 #include <osmocom/core/timer.h>
 
+#include <osmocom/bb/l1sched/prim.h>
+
 #define GPRS_L2_MAX_LEN		54
 #define EDGE_L2_MAX_LEN		155
 
@@ -47,12 +49,6 @@ enum l1sched_clck_state {
 enum l1sched_burst_type {
 	L1SCHED_BURST_GMSK,
 	L1SCHED_BURST_8PSK,
-};
-
-enum l1sched_ts_prim_type {
-	L1SCHED_PRIM_DATA,
-	L1SCHED_PRIM_RACH8,
-	L1SCHED_PRIM_RACH11,
 };
 
 /**
@@ -101,29 +97,6 @@ enum l1sched_lchan_type {
 	L1SCHED_SDCCH4_CBCH,
 	L1SCHED_SDCCH8_CBCH,
 	_L1SCHED_CHAN_MAX
-};
-
-enum l1sched_data_type {
-	L1SCHED_DT_PACKET_DATA,
-	L1SCHED_DT_SIGNALING,
-	L1SCHED_DT_TRAFFIC,
-	L1SCHED_DT_OTHER, /* SCH and RACH */
-};
-
-enum l1sched_config_type {
-	/*! Channel combination for a timeslot */
-	L1SCHED_CFG_PCHAN_COMB,
-};
-
-/* Represents a (re)configuration request */
-struct l1sched_config_req {
-	enum l1sched_config_type type;
-	union {
-		struct {
-			uint8_t tn;
-			enum gsm_phys_chan_config pchan;
-		} pchan_comb;
-	};
 };
 
 /* Represents a burst to be transmitted */
@@ -182,10 +155,6 @@ struct l1sched_lchan_desc {
 	uint8_t chan_nr;
 	/*! Link ID (like in RSL) */
 	uint8_t link_id;
-	/*! Sub-slot number (for SDCCH and TCH/H) */
-	uint8_t ss_nr;
-	/*! GSMTAP channel type (see GSMTAP_CHANNEL_*) */
-	uint8_t gsmtap_chan_type;
 
 	/*! How much memory do we need to store bursts */
 	size_t burst_buf_size;
@@ -259,8 +228,10 @@ struct l1sched_lchan_state {
 	/*! Burst buffer for TX */
 	ubit_t *tx_bursts;
 
-	/*! A primitive being sent */
-	struct l1sched_ts_prim *prim;
+	/*! Queue of Tx primitives */
+	struct llist_head tx_prims;
+	/*! Tx primitive being sent */
+	struct msgb *prim;
 
 	/*! Mode for TCH channels (see GSM48_CMODE_*) */
 	uint8_t	tch_mode;
@@ -340,36 +311,8 @@ struct l1sched_ts {
 	const struct l1sched_tdma_multiframe *mf_layout;
 	/*! Channel states for logical channels */
 	struct llist_head lchans;
-	/*! Queue primitives for TX */
-	struct llist_head tx_prims;
 	/*! Backpointer to the scheduler */
 	struct l1sched_state *sched;
-};
-
-/* Represents one TX primitive in the queue of l1sched_ts */
-struct l1sched_ts_prim {
-	/*! Link to queue of TS */
-	struct llist_head list;
-	/*! Type of primitive */
-	enum l1sched_ts_prim_type type;
-	/*! Logical channel type */
-	enum l1sched_lchan_type chan;
-	/*! TDMA Fn for L1SCHED_{PDTCH,PTCCH} */
-	uint32_t fn;
-	/*! Payload length */
-	size_t payload_len;
-	/*! Payload */
-	uint8_t payload[0];
-};
-
-/*! Represents a RACH (8-bit or 11-bit) primitive */
-struct l1sched_ts_prim_rach {
-	/*! RA value */
-	uint16_t ra;
-	/*! Training Sequence (only for 11-bit RA) */
-	uint8_t synch_seq;
-	/*! Transmission offset (how many frames to skip) */
-	uint8_t offset;
 };
 
 /*! Scheduler configuration */
@@ -426,8 +369,6 @@ int l1sched_start_ciphering(struct l1sched_ts *ts, uint8_t algo,
 
 /* Logical channel management functions */
 enum gsm_phys_chan_config l1sched_chan_nr2pchan_config(uint8_t chan_nr);
-enum l1sched_lchan_type l1sched_chan_nr2lchan_type(uint8_t chan_nr,
-	uint8_t link_id);
 
 void l1sched_deactivate_all_lchans(struct l1sched_ts *ts);
 int l1sched_set_lchans(struct l1sched_ts *ts, uint8_t chan_nr,
@@ -440,12 +381,6 @@ struct l1sched_lchan_state *l1sched_find_lchan_by_type(struct l1sched_ts *ts,
 						       enum l1sched_lchan_type type);
 struct l1sched_lchan_state *l1sched_find_lchan_by_chan_nr(struct l1sched_state *sched,
 							  uint8_t chan_nr, uint8_t link_id);
-
-/* Primitive management functions */
-struct l1sched_ts_prim *l1sched_prim_push(struct l1sched_state *sched,
-					  enum l1sched_ts_prim_type type,
-					  uint8_t chan_nr, uint8_t link_id,
-					  const uint8_t *pl, size_t pl_len);
 
 #define L1SCHED_TCH_MODE_IS_SPEECH(mode)   \
 	  (mode == GSM48_CMODE_SPEECH_V1   \
@@ -464,31 +399,12 @@ struct l1sched_ts_prim *l1sched_prim_push(struct l1sched_state *sched,
 #define L1SCHED_CHAN_IS_SACCH(chan) \
 	(l1sched_lchan_desc[chan].link_id & L1SCHED_CH_LID_SACCH)
 
-#define L1SCHED_PRIM_IS_RACH11(prim) \
-	(prim->type == L1SCHED_PRIM_RACH11)
-
-#define L1SCHED_PRIM_IS_RACH8(prim) \
-	(prim->type == L1SCHED_PRIM_RACH8)
-
-#define L1SCHED_PRIM_IS_RACH(prim) \
-	(L1SCHED_PRIM_IS_RACH8(prim) || L1SCHED_PRIM_IS_RACH11(prim))
-
-#define L1SCHED_PRIM_IS_TCH(prim) \
-	(L1SCHED_CHAN_IS_TCH(prim->chan) && prim->payload_len != GSM_MACBLOCK_LEN)
-
-#define L1SCHED_PRIM_IS_FACCH(prim) \
-	(L1SCHED_CHAN_IS_TCH(prim->chan) && prim->payload_len == GSM_MACBLOCK_LEN)
-
-struct l1sched_ts_prim *l1sched_prim_dequeue(struct llist_head *queue,
-	uint32_t fn, struct l1sched_lchan_state *lchan);
-int l1sched_prim_dummy(struct l1sched_lchan_state *lchan);
-void l1sched_lchan_prim_drop(struct l1sched_lchan_state *lchan);
-void l1sched_prim_flush_queue(struct llist_head *list);
-
 int l1sched_handle_rx_burst(struct l1sched_state *sched,
 			    struct l1sched_burst_ind *bi);
 int l1sched_handle_rx_probe(struct l1sched_state *sched,
 			    struct l1sched_probe *probe);
+int l1sched_handle_burst_req(struct l1sched_state *sched,
+			     const struct l1sched_burst_req *br);
 
 /* Shared declarations for lchan handlers */
 extern const uint8_t l1sched_nb_training_bits[8][26];
@@ -521,17 +437,3 @@ void l1sched_clck_reset(struct l1sched_state *sched);
 
 void l1sched_pull_burst(struct l1sched_state *sched, struct l1sched_burst_req *br);
 void l1sched_pull_send_frame(struct l1sched_state *sched);
-
-/* External L1 API, must be implemented by the API user */
-int l1sched_handle_config_req(struct l1sched_state *sched,
-			      const struct l1sched_config_req *cr);
-int l1sched_handle_burst_req(struct l1sched_state *sched,
-			     const struct l1sched_burst_req *br);
-
-/* External L2 API, must be implemented by the API user */
-int l1sched_handle_data_ind(struct l1sched_lchan_state *lchan,
-			    const uint8_t *data, size_t data_len,
-			    int n_errors, int n_bits_total,
-			    enum l1sched_data_type dt);
-int l1sched_handle_data_cnf(struct l1sched_lchan_state *lchan,
-			    uint32_t fn, enum l1sched_data_type dt);

commit 7ce8cdd32543312f496824bf82ebbaf565a01b6f

trxcon/l1sched: allocate primitives of fixed size (64 + 64)
When running trxcon with GSMTAP Um logging enabled (-g cmd line arg),
in handle_prim_rach_cnf() we msgb_put() one or two bytes to the given
msgb.  This causes a segfault, because the L1SCHED_PRIM_T_RACH prims
have 0 tailroom bytes available.

While we could allocate L1SCHED_PRIM_T_RACH with a few extra bytes,
a more fundamental approach is to allocate all l1sched primitives with
a fixed tailroom.

Change-Id: Ica87b147e11744a69dcd7c056376dcf6b98f9ca6
Fixes: ff9db9de "trxcon/l1sched: rework the primitive API"
Related: OS#5500

src/host/trxcon/include/osmocom/bb/l1sched/prim.h
@@ -114,8 +114,7 @@ void l1sched_prim_init(struct msgb *msg,
 		       enum osmo_prim_operation op);
 
 struct msgb *l1sched_prim_alloc(enum l1sched_prim_type type,
-				enum osmo_prim_operation op,
-				size_t extra_size);
+				enum osmo_prim_operation op);
 
 struct msgb *l1sched_lchan_prim_dequeue(struct l1sched_lchan_state *lchan, uint32_t fn);
 void l1sched_lchan_prim_assign_dummy(struct l1sched_lchan_state *lchan);

src/host/trxcon/src/sched_lchan_sch.c
@@ -66,7 +66,7 @@ static int handle_sch_ind(struct l1sched_state *sched, uint32_t fn, uint8_t bsic
 	struct l1sched_prim *prim;
 	struct msgb *msg;
 
-	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_SCH, PRIM_OP_INDICATION, 0);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_SCH, PRIM_OP_INDICATION);
 	OSMO_ASSERT(msg != NULL);
 
 	prim = l1sched_prim_from_msgb(msg);

src/host/trxcon/src/sched_prim.c
@@ -35,7 +35,8 @@
 #include <osmocom/bb/l1sched/l1sched.h>
 #include <osmocom/bb/l1sched/logging.h>
 
-#define L1SCHED_PRIM_HEADROOM	128
+#define L1SCHED_PRIM_HEADROOM	64
+#define L1SCHED_PRIM_TAILROOM	64
 
 osmo_static_assert(sizeof(struct l1sched_prim) <= L1SCHED_PRIM_HEADROOM, l1sched_prim_size);
 
@@ -61,12 +62,11 @@ void l1sched_prim_init(struct msgb *msg,
 }
 
 struct msgb *l1sched_prim_alloc(enum l1sched_prim_type type,
-				enum osmo_prim_operation op,
-				size_t extra_size)
+				enum osmo_prim_operation op)
 {
 	struct msgb *msg;
 
-	msg = msgb_alloc_headroom(L1SCHED_PRIM_HEADROOM + extra_size,
+	msg = msgb_alloc_headroom(L1SCHED_PRIM_HEADROOM + L1SCHED_PRIM_TAILROOM,
 				  L1SCHED_PRIM_HEADROOM, "l1sched_prim");
 	if (msg == NULL)
 		return NULL;
@@ -89,7 +89,7 @@ static struct msgb *prim_compose_mr(struct l1sched_lchan_state *lchan)
 	bool cached;
 
 	/* Allocate a new primitive */
-	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST, GSM_MACBLOCK_LEN);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST);
 	OSMO_ASSERT(msg != NULL);
 
 	prim = l1sched_prim_from_msgb(msg);
@@ -469,7 +469,7 @@ void l1sched_lchan_prim_assign_dummy(struct l1sched_lchan_state *lchan)
 	if (!prim_len)
 		return;
 
-	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST, prim_len);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST);
 	OSMO_ASSERT(msg != NULL);
 
 	prim = l1sched_prim_from_msgb(msg);
@@ -498,7 +498,7 @@ int l1sched_lchan_emit_data_ind(struct l1sched_lchan_state *lchan,
 
 	lchan_desc = &l1sched_lchan_desc[lchan->type];
 
-	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_INDICATION, data_len);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_INDICATION);
 	OSMO_ASSERT(msg != NULL);
 
 	prim = l1sched_prim_from_msgb(msg);

src/host/trxcon/src/sched_trx.c
@@ -74,7 +74,7 @@ static int l1sched_cfg_pchan_comb_ind(struct l1sched_state *sched,
 	struct l1sched_prim *prim;
 	struct msgb *msg;
 
-	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_PCHAN_COMB, PRIM_OP_INDICATION, 0);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_PCHAN_COMB, PRIM_OP_INDICATION);
 	OSMO_ASSERT(msg != NULL);
 
 	prim = l1sched_prim_from_msgb(msg);

src/host/trxcon/src/trxcon_fsm.c
@@ -286,7 +286,7 @@ static void handle_tx_access_burst_req(struct osmo_fsm_inst *fi,
 	struct l1sched_prim *prim;
 	struct msgb *msg;
 
-	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_RACH, PRIM_OP_REQUEST, 0);
+	msg = l1sched_prim_alloc(L1SCHED_PRIM_T_RACH, PRIM_OP_REQUEST);
 	OSMO_ASSERT(msg != NULL);
 
 	prim = l1sched_prim_from_msgb(msg);
@@ -492,7 +492,7 @@ static void trxcon_st_dedicated_action(struct osmo_fsm_inst *fi,
 		struct l1sched_prim *prim;
 		struct msgb *msg;
 
-		msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST, req->data_len);
+		msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST);
 		OSMO_ASSERT(msg != NULL);
 
 		prim = l1sched_prim_from_msgb(msg);
@@ -563,7 +563,7 @@ static void trxcon_st_packet_data_action(struct osmo_fsm_inst *fi,
 		if (l1gprs_handle_ul_block_req(trxcon->gprs, &block_req, msg) != 0)
 			return;
 
-		msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST, block_req.data_len);
+		msg = l1sched_prim_alloc(L1SCHED_PRIM_T_DATA, PRIM_OP_REQUEST);
 		OSMO_ASSERT(msg != NULL);
 
 		prim = l1sched_prim_from_msgb(msg);