osmocom/osmocom-bb

commit 67943df4b7e59ef4982b586271566099c2f14d4a

layer23: fix parsing of command line options
After the recent refactoring, parsing of the command line options is
broken for some arguments.  Specifically, the value of '-a'/'--arfcn'
is ignored and hard-coded ARFCN=871 is used instead.

The problem is that l23_app_init(), which allocates an MS state and
sets the initial ARFCN, is called *before* handle_options().  So the
cfg_test_arfcn is used before it gets overwritten from the argv[].

The usual approach in osmo-* apps is to parse the command line
arguments first, and only then execute code which depends on
configurable parameters.  Let's follow this approach too.

Change-Id: I77ca11c14561fa3fcb9add60ccea5b0b847a20c4

src/host/layer23/src/common/main.c
@@ -254,13 +254,13 @@ int main(int argc, char **argv)
 
 	print_copyright();
 
+	handle_options(argc, argv);
+
 	rc = l23_app_init();
 	if (rc < 0) {
 		fprintf(stderr, "Failed during l23_app_init()\n");
 		exit(1);
 	}
-
-	handle_options(argc, argv);
 
 	if (l23_app_info.opt_supported & L23_OPT_VTY) {
 		if (_vty_init() < 0)

src/host/layer23/src/mobile/main.c
@@ -255,6 +255,12 @@ int main(int argc, char **argv)
 
 	print_copyright();
 
+	rc = handle_options(argc, argv);
+	if (rc) { /* Abort in case of parsing errors */
+		fprintf(stderr, "Error in command line options. Exiting.\n");
+		return 1;
+	}
+
 	srand(time(NULL));
 
 	INIT_LLIST_HEAD(&ms_list);
@@ -270,12 +276,6 @@ int main(int argc, char **argv)
 	if (rc < 0) {
 		fprintf(stderr, "Failed during l23_app_init()\n");
 		exit(1);
-	}
-
-	rc = handle_options(argc, argv);
-	if (rc) { /* Abort in case of parsing errors */
-		fprintf(stderr, "Error in command line options. Exiting.\n");
-		return 1;
 	}
 
 	if (custom_cfg_file) {

commit 749f0a461cc78b0fbd17f3eeee36dace4ba9c8be

layer23: fix handling of logging category mask (-d option)
In change 67943df4 I broke handling of the logging category mask in
the mobile app.  Adding this option results in a segfault:

  ERROR: osmo_log_info == NULL! You must call log_init() before
         using logging in log_parse_category_mask()!
  Assert failed osmo_log_info src/libosmocore/src/core/logging.c:329

As can be seen, the problem is that we are calling
log_parse_category_mask() before initializing the logging.

As possible solution, I could rearrange the code to parse command
line options after calling osmo_init_logging2().  This would fix
the segfault, but would not fully solve the problem.

If we call log_parse_category_mask() before parsing the config file,
then logging configuration in the config file overwrites the logging
configuration specified via the command line.  But we want the
opposite: the command line setting should overwrite the config file
parameters.  This is handy because there is no need to edit the
config file if you quickly need to test something.

So let's call log_parse_category_mask() after parsing the config file.

Change-Id: I1b2b7804bf99b71f96e9197f7824cfd20431e8a1
Fixes: 67943df4 "layer23: fix parsing of command line options"

src/host/layer23/src/common/main.c
@@ -58,6 +58,7 @@ static char *sap_socket_path = "/tmp/osmocom_sap";
 struct llist_head ms_list;
 static char *gsmtap_ip = NULL;
 static char *config_file = NULL;
+static char *log_cat_mask = NULL;
 
 int (*l23_app_start)(void) = NULL;
 int (*l23_app_work)(void) = NULL;
@@ -162,7 +163,7 @@ static void handle_options(int argc, char **argv)
 			config_file = optarg;
 			break;
 		case 'd':
-			log_parse_category_mask(osmo_stderr_target, optarg);
+			log_cat_mask = optarg;
 			break;
 		default:
 			if (l23_app_info.cfg_handle_opt != NULL)
@@ -267,6 +268,9 @@ int main(int argc, char **argv)
 		if (_vty_init() < 0)
 			exit(1);
 	}
+
+	if (log_cat_mask != NULL)
+		log_parse_category_mask(osmo_stderr_target, log_cat_mask);
 
 	if (l23_app_info.opt_supported & L23_OPT_TAP) {
 		if (gsmtap_ip) {

src/host/layer23/src/mobile/main.c
@@ -54,6 +54,7 @@ void *l23_ctx = NULL;
 struct l23_global_config l23_cfg;
 struct llist_head ms_list;
 static const char *custom_cfg_file = NULL;
+static const char *log_cat_mask = NULL;
 static char *config_file = NULL;
 char *config_dir = NULL;
 int daemonize = 0;
@@ -118,7 +119,7 @@ static int handle_options(int argc, char **argv)
 			custom_cfg_file = optarg;
 			break;
 		case 'd':
-			log_parse_category_mask(osmo_stderr_target, optarg);
+			log_cat_mask = optarg;
 			break;
 		case 'D':
 			daemonize = 1;
@@ -296,6 +297,9 @@ int main(int argc, char **argv)
 		if (_vty_init() < 0)
 			exit(1);
 	}
+
+	if (log_cat_mask != NULL)
+		log_parse_category_mask(osmo_stderr_target, log_cat_mask);
 
 	if (l23_cfg.gsmtap.remote_host) {
 		l23_cfg.gsmtap.inst = gsmtap_source_init2(l23_cfg.gsmtap.local_host, 0,