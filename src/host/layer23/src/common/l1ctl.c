9f61426a15cfde452a15c338238c35df76d09d56
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2023-06-23 00:01:22 +0700

    layer23: use gsm0502_fn2ccch_block() from libosmogsm
    Change-Id: I652a2ab9ddb3cb5082cce911a5cdb4cf8abcf552
    Depends: libosmocore.git I8cbd31226754e95887358ed83a928e2f567f4cf3

--- src/host/layer23/src/common/l1ctl.c
+++ src/host/layer23/src/common/l1ctl.c
@@ -38,6 +38,7 @@
 #include <osmocom/gsm/protocol/gsm_08_58.h>
 #include <osmocom/gsm/rsl.h>
 #include <osmocom/gsm/lapdm.h>
+#include <osmocom/gsm/gsm0502.h>
 
 #include <osmocom/bb/common/l1ctl.h>
 #include <osmocom/bb/common/l23_app.h>
@@ -46,35 +47,10 @@
 #include <osmocom/bb/common/l1l2_interface.h>
 #include <osmocom/bb/common/logging.h>
 
-#define CB_FCCH		-1
-#define CB_SCH		-2
-#define CB_BCCH		-3
-#define CB_IDLE		-4
-
-/* according to TS 05.02 Clause 7 Table 3 of 9 an Figure 8a */
-static const int ccch_block_table[51] = {
-	CB_FCCH, CB_SCH,/* 0..1 */
-	CB_BCCH, CB_BCCH, CB_BCCH, CB_BCCH,	/* 2..5: BCCH */
-	0, 0, 0, 0,	/* 6..9: B0 */
-	CB_FCCH, CB_SCH,/* 10..11 */
-	1, 1, 1, 1,	/* 12..15: B1 */
-	2, 2, 2, 2,	/* 16..19: B2 */
-	CB_FCCH, CB_SCH,/* 20..21 */
-	3, 3, 3, 3,	/* 22..25: B3 */
-	4, 4, 4, 4,	/* 26..29: B4 */
-	CB_FCCH, CB_SCH,/* 30..31 */
-	5, 5, 5, 5,	/* 32..35: B5 */
-	6, 6, 6, 6,	/* 36..39: B6 */
-	CB_FCCH, CB_SCH,/* 40..41 */
-	7, 7, 7, 7, 	/* 42..45: B7 */
-	8, 8, 8, 8, 	/* 46..49: B8 */
-	-4		/* 50: Idle */
-};
-
 /* determine the CCCH block number based on the frame number */
 static unsigned int fn2ccch_block(uint32_t fn)
 {
-	int rc = ccch_block_table[fn%51];
+	int rc = gsm0502_fn2ccch_block(fn);
 	/* if FN is negative, we were called for something that's not CCCH! */
 	OSMO_ASSERT(rc >= 0);
 	return rc;
