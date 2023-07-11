osmocom/osmocom-bb

commit ad8f7794c9b7c5c03f34e1d6a273e8b5f7c9da30

trxcon/l1sched: remove redundant TCH/[FH] prim length checks
Both gsm0503_tch_[fh]r_encode() do check the given payload length in
order to determine the payload and/or codec type.  The same applies
to gsm0503_tch_a[fh]s_encode().  There is no real need to implement
additional length checks on top of that - drop them.

Change-Id: Ib1adf4945fb762bc2a51a1008f6bef6784fb7833
Related: OS#4396

src/host/trxcon/src/sched_lchan_tchf.c
@@ -219,7 +219,6 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 	ubit_t *buffer, *offset;
 	const uint8_t *tsc;
 	uint8_t *mask;
-	size_t l2_len;
 	int rc;
 
 	/* Set up pointers */
@@ -238,10 +237,16 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 	memcpy(buffer, buffer + 464, 464);
 
 	/* populate the buffer with bursts */
-	if (msgb_l2len(lchan->prim) == GSM_MACBLOCK_LEN) {
-		/* Encode payload */
-		rc = gsm0503_tch_fr_encode(buffer, msgb_l2(lchan->prim), GSM_MACBLOCK_LEN, 1);
-	} else if (lchan->tch_mode == GSM48_CMODE_SPEECH_AMR) {
+	switch (lchan->tch_mode) {
+	case GSM48_CMODE_SIGN:
+	case GSM48_CMODE_SPEECH_V1:
+	case GSM48_CMODE_SPEECH_EFR:
+		rc = gsm0503_tch_fr_encode(buffer,
+					   msgb_l2(lchan->prim),
+					   msgb_l2len(lchan->prim), 1);
+		break;
+	case GSM48_CMODE_SPEECH_AMR:
+	{
 		int len;
 		uint8_t cmr_codec;
 		int ft, cmr, i;
@@ -295,31 +300,11 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 					    lchan->amr.codecs,
 					    lchan->amr.ul_ft,
 					    lchan->amr.ul_cmr);
-	} else {
-		/* Determine and check the payload length */
-		switch (lchan->tch_mode) {
-		case GSM48_CMODE_SIGN:
-		case GSM48_CMODE_SPEECH_V1: /* FR */
-			l2_len = GSM_FR_BYTES;
-			break;
-		case GSM48_CMODE_SPEECH_EFR: /* EFR */
-			l2_len = GSM_EFR_BYTES;
-			break;
-		default:
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Invalid TCH mode: %u, dropping frame...\n",
-				    lchan->tch_mode);
-			l1sched_lchan_prim_drop(lchan);
-			return -EINVAL;
-		}
-		if (msgb_l2len(lchan->prim) != l2_len) {
-			LOGP_LCHAND(lchan, LOGL_ERROR, "Primitive has odd length %u "
-				    "(expected %zu for TCH or %u for FACCH), so dropping...\n",
-				    msgb_l2len(lchan->prim), l2_len, GSM_MACBLOCK_LEN);
-			l1sched_lchan_prim_drop(lchan);
-			return -EINVAL;
-		}
-		rc = gsm0503_tch_fr_encode(buffer, msgb_l2(lchan->prim), l2_len, 1);
+		break;
+	}
+	default:
+		LOGP_LCHAND(lchan, LOGL_ERROR, "Invalid TCH mode: %u\n", lchan->tch_mode);
+		return -EINVAL;
 	}
 
 	if (rc) {

src/host/trxcon/src/sched_lchan_tchh.c
@@ -425,7 +425,6 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 	ubit_t *buffer, *offset;
 	const uint8_t *tsc;
 	uint8_t *mask;
-	size_t l2_len;
 	int rc;
 
 	/* Set up pointers */
@@ -459,10 +458,15 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 	}
 
 	/* populate the buffer with bursts */
-	if (msgb_l2len(lchan->prim) == GSM_MACBLOCK_LEN) {
-		rc = gsm0503_tch_hr_encode(buffer, msgb_l2(lchan->prim), GSM_MACBLOCK_LEN);
-		lchan->ul_facch_blocks = 6;
-	} else if (lchan->tch_mode == GSM48_CMODE_SPEECH_AMR) {
+	switch (lchan->tch_mode) {
+	case GSM48_CMODE_SIGN:
+	case GSM48_CMODE_SPEECH_V1:
+		rc = gsm0503_tch_hr_encode(buffer,
+					   msgb_l2(lchan->prim),
+					   msgb_l2len(lchan->prim));
+		break;
+	case GSM48_CMODE_SPEECH_AMR:
+	{
 		int len;
 		uint8_t cmr_codec;
 		int ft, cmr, i;
@@ -516,28 +520,11 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 					    lchan->amr.codecs,
 					    lchan->amr.ul_ft,
 					    lchan->amr.ul_cmr);
-	} else {
-		/* Determine and check the payload length */
-		switch (lchan->tch_mode) {
-		case GSM48_CMODE_SIGN:
-		case GSM48_CMODE_SPEECH_V1: /* HR */
-			l2_len = GSM_HR_BYTES + 1;
-			break;
-		default:
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Invalid TCH mode: %u, dropping frame...\n",
-				    lchan->tch_mode);
-			l1sched_lchan_prim_drop(lchan);
-			return -EINVAL;
-		}
-		if (msgb_l2len(lchan->prim) != l2_len) {
-			LOGP_LCHAND(lchan, LOGL_ERROR, "Primitive has odd length %u "
-				    "(expected %zu for TCH or %u for FACCH), so dropping...\n",
-				    msgb_l2len(lchan->prim), l2_len, GSM_MACBLOCK_LEN);
-			l1sched_lchan_prim_drop(lchan);
-			return -EINVAL;
-		}
-		rc = gsm0503_tch_hr_encode(buffer, msgb_l2(lchan->prim), l2_len);
+		break;
+	}
+	default:
+		LOGP_LCHAND(lchan, LOGL_ERROR, "Invalid TCH mode: %u\n", lchan->tch_mode);
+		return -EINVAL;
 	}
 
 	if (rc) {

commit d84b2ed6eda89d8e055d0073b50e3cc6e2b31a95

trxcon/l1sched: fix handling of UL FACCH on TCH/A[FH]S
In ad8f7794 I changed both tx_tch[fh]_fn() to use a switch statement
and introduced a regression by removing special treatment of FACCH:

@@ -238,10 +237,16 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
-       if (msgb_l2len(lchan->prim) == GSM_MACBLOCK_LEN) {
-               /* Encode payload */
-               rc = gsm0503_tch_fr_encode(buffer, msgb_l2(lchan->prim), GSM_MACBLOCK_LEN, 1);
-       } else if (lchan->tch_mode == GSM48_CMODE_SPEECH_AMR) {
@@ -459,10 +458,15 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
-       if (msgb_l2len(lchan->prim) == GSM_MACBLOCK_LEN) {
-               rc = gsm0503_tch_hr_encode(buffer, msgb_l2(lchan->prim), GSM_MACBLOCK_LEN);
-               lchan->ul_facch_blocks = 6;
-       } else if (lchan->tch_mode == GSM48_CMODE_SPEECH_AMR) {

Now if the channel mode is GSM48_CMODE_SPEECH_AMR, UL FACCH/[FH] frames
will be fed to osmo_amr_rtp_dec(), which is definitely wrong.  Fix this
by doing all AMR specific checks in a separate function, which is
called only for speech frames.

Change-Id: Ie217bbb389b5abb95d241781ffe3f5c7b1c188c0
Fixes: ad8f7794 "trxcon/l1sched: remove redundant TCH/[FH] prim length checks"
Related: OS#4396

src/host/trxcon/include/osmocom/bb/l1sched/l1sched.h
@@ -392,6 +392,7 @@ extern const uint8_t l1sched_nb_training_bits[8][26];
 
 const char *l1sched_burst_mask2str(const uint8_t *mask, int bits);
 size_t l1sched_bad_frame_ind(uint8_t *l2, struct l1sched_lchan_state *lchan);
+bool l1sched_lchan_amr_prim_is_valid(struct l1sched_lchan_state *lchan, bool is_cmr);
 
 /* Interleaved TCH/H block TDMA frame mapping */
 bool l1sched_tchh_block_map_fn(enum l1sched_lchan_type chan,

src/host/trxcon/src/sched_lchan_common.c
@@ -141,3 +141,48 @@ size_t l1sched_bad_frame_ind(uint8_t *l2, struct l1sched_lchan_state *lchan)
 		return 0;
 	}
 }
+
+bool l1sched_lchan_amr_prim_is_valid(struct l1sched_lchan_state *lchan, bool is_cmr)
+{
+	enum osmo_amr_type ft_codec;
+	enum osmo_amr_quality bfi;
+	uint8_t cmr_codec;
+	int ft, cmr, len;
+	int8_t sti, cmi;
+
+	len = osmo_amr_rtp_dec(msgb_l2(lchan->prim), msgb_l2len(lchan->prim),
+			       &cmr_codec, &cmi, &ft_codec, &bfi, &sti);
+	if (len < 0) {
+		LOGP_LCHAND(lchan, LOGL_ERROR, "Cannot send invalid AMR payload (%u): %s\n",
+			    msgb_l2len(lchan->prim), msgb_hexdump_l2(lchan->prim));
+		return false;
+	}
+	ft = -1;
+	cmr = -1;
+	for (unsigned int i = 0; i < lchan->amr.codecs; i++) {
+		if (lchan->amr.codec[i] == ft_codec)
+			ft = i;
+		if (lchan->amr.codec[i] == cmr_codec)
+			cmr = i;
+	}
+	if (ft < 0) {
+		LOGP_LCHAND(lchan, LOGL_ERROR,
+			    "Codec (FT = %d) of RTP frame not in list\n", ft_codec);
+		return false;
+	}
+	if (is_cmr && lchan->amr.ul_ft != ft) {
+		LOGP_LCHAND(lchan, LOGL_ERROR,
+			    "Codec (FT = %d) of RTP cannot be changed now, but in next frame\n",
+			    ft_codec);
+		return false;
+	}
+	lchan->amr.ul_ft = ft;
+	if (cmr < 0) {
+		LOGP_LCHAND(lchan, LOGL_ERROR,
+			    "Codec (CMR = %d) of RTP frame not in list\n", cmr_codec);
+	} else {
+		lchan->amr.ul_cmr = cmr;
+	}
+
+	return true;
+}

src/host/trxcon/src/sched_lchan_tchf.c
@@ -244,54 +244,20 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 		break;
 	case GSM48_CMODE_SPEECH_AMR:
 	{
-		int len;
-		uint8_t cmr_codec;
-		int ft, cmr, i;
-		enum osmo_amr_type ft_codec;
-		enum osmo_amr_quality bfi;
-		int8_t sti, cmi;
-		bool amr_fn_is_cmr;
-		/* the first FN 0,8,17 defines that CMI is included in frame,
-		 * the first FN 4,13,21 defines that CMR is included in frame.
-		 */
-		amr_fn_is_cmr = !sched_tchf_ul_amr_cmi_map[br->fn % 26];
+		bool amr_fn_is_cmr = !sched_tchf_ul_amr_cmi_map[br->fn % 26];
+		const uint8_t *data = msgb_l2(lchan->prim);
+		size_t data_len = msgb_l2len(lchan->prim);
 
-		len = osmo_amr_rtp_dec(msgb_l2(lchan->prim), msgb_l2len(lchan->prim),
-				       &cmr_codec, &cmi, &ft_codec, &bfi, &sti);
-		if (len < 0) {
-			LOGP_LCHAND(lchan, LOGL_ERROR, "Cannot send invalid AMR payload (%u): %s\n",
-				    msgb_l2len(lchan->prim), msgb_hexdump_l2(lchan->prim));
-			goto free_bad_msg;
+		if (data_len != GSM_MACBLOCK_LEN) { /* TCH/AFS: speech */
+			if (!l1sched_lchan_amr_prim_is_valid(lchan, amr_fn_is_cmr))
+				goto free_bad_msg;
+			/* pull the AMR header - sizeof(struct amr_hdr) */
+			data_len -= 2;
+			data += 2;
 		}
-		ft = -1;
-		cmr = -1;
-		for (i = 0; i < lchan->amr.codecs; i++) {
-			if (lchan->amr.codec[i] == ft_codec)
-				ft = i;
-			if (lchan->amr.codec[i] == cmr_codec)
-				cmr = i;
-		}
-		if (ft < 0) {
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Codec (FT = %d) of RTP frame not in list\n", ft_codec);
-			goto free_bad_msg;
-		}
-		if (amr_fn_is_cmr && lchan->amr.ul_ft != ft) {
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Codec (FT = %d) of RTP cannot be changed now, but in next frame\n",
-				    ft_codec);
-			goto free_bad_msg;
-		}
-		lchan->amr.ul_ft = ft;
-		if (cmr < 0) {
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Codec (CMR = %d) of RTP frame not in list\n", cmr_codec);
-		} else {
-			lchan->amr.ul_cmr = cmr;
-		}
+
 		rc = gsm0503_tch_afs_encode(bursts_p,
-					    msgb_l2(lchan->prim) + 2,
-					    msgb_l2len(lchan->prim) - 2,
+					    data, data_len,
 					    amr_fn_is_cmr,
 					    lchan->amr.codec,
 					    lchan->amr.codecs,

src/host/trxcon/src/sched_lchan_tchh.c
@@ -457,54 +457,20 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 		break;
 	case GSM48_CMODE_SPEECH_AMR:
 	{
-		int len;
-		uint8_t cmr_codec;
-		int ft, cmr, i;
-		enum osmo_amr_type ft_codec;
-		enum osmo_amr_quality bfi;
-		int8_t sti, cmi;
-		bool amr_fn_is_cmr;
-		/* the first FN 0,8,17 defines that CMI is included in frame,
-		 * the first FN 4,13,21 defines that CMR is included in frame.
-		 */
-		amr_fn_is_cmr = !sched_tchh_ul_amr_cmi_map[br->fn % 26];
+		bool amr_fn_is_cmr = !sched_tchh_ul_amr_cmi_map[br->fn % 26];
+		const uint8_t *data = msgb_l2(lchan->prim);
+		size_t data_len = msgb_l2len(lchan->prim);
 
-		len = osmo_amr_rtp_dec(msgb_l2(lchan->prim), msgb_l2len(lchan->prim),
-				       &cmr_codec, &cmi, &ft_codec, &bfi, &sti);
-		if (len < 0) {
-			LOGP_LCHAND(lchan, LOGL_ERROR, "Cannot send invalid AMR payload (%u): %s\n",
-				    msgb_l2len(lchan->prim), msgb_hexdump_l2(lchan->prim));
-			goto free_bad_msg;
+		if (data_len != GSM_MACBLOCK_LEN) { /* TCH/AHS: speech */
+			if (!l1sched_lchan_amr_prim_is_valid(lchan, amr_fn_is_cmr))
+				goto free_bad_msg;
+			/* pull the AMR header - sizeof(struct amr_hdr) */
+			data_len -= 2;
+			data += 2;
 		}
-		ft = -1;
-		cmr = -1;
-		for (i = 0; i < lchan->amr.codecs; i++) {
-			if (lchan->amr.codec[i] == ft_codec)
-				ft = i;
-			if (lchan->amr.codec[i] == cmr_codec)
-				cmr = i;
-		}
-		if (ft < 0) {
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Codec (FT = %d) of RTP frame not in list\n", ft_codec);
-			goto free_bad_msg;
-		}
-		if (amr_fn_is_cmr && lchan->amr.ul_ft != ft) {
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Codec (FT = %d) of RTP cannot be changed now, but in next frame\n",
-				    ft_codec);
-			goto free_bad_msg;
-		}
-		lchan->amr.ul_ft = ft;
-		if (cmr < 0) {
-			LOGP_LCHAND(lchan, LOGL_ERROR,
-				    "Codec (CMR = %d) of RTP frame not in list\n", cmr_codec);
-		} else {
-			lchan->amr.ul_cmr = cmr;
-		}
+
 		rc = gsm0503_tch_ahs_encode(bursts_p,
-					    msgb_l2(lchan->prim) + 2,
-					    msgb_l2len(lchan->prim) - 2,
+					    data, data_len,
 					    amr_fn_is_cmr,
 					    lchan->amr.codec,
 					    lchan->amr.codecs,