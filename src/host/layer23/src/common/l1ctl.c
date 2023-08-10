osmocom/osmocom-bb

commit d524c17d907b39189c1c7b465b8f40db2d79f55b

l1ctl: Fill ph_data_param fn field
Related: OS#3626
Change-Id: I3317296b2bda1088a2af81ba9b135714f8393afd

src/host/layer23/src/common/l1ctl.c
@@ -317,6 +317,7 @@ static int rx_ph_data_ind(struct osmocom_ms *ms, struct msgb *msg)
 			PRIM_OP_INDICATION, msg);
 	pp.u.data.chan_nr = dl->chan_nr;
 	pp.u.data.link_id = dl->link_id;
+	pp.u.data.fn = dl->frame_nr;
 
 	/* send it up into LAPDm */
 	return lapdm_phsap_up(&pp.oph, le);

commit 8bbd0d173fad3708fac3207d56dd04c14912351e

l1ctl: Fix fill ph_data_param fn field
This commit fixes recent previous commit filling in the fn field. The
dl->frame_nr is network order, and we want to pass a host order integer
in the primitive. Use the tm.fn which already includes the proper value
calculated from dl->frame_nr.

Fixes: d524c17d907b39189c1c7b465b8f40db2d79f55b
Related: OS#3626
Change-Id: Id96015c8b419932abb8095c6cb85aceef34e366f

src/host/layer23/src/common/l1ctl.c
@@ -317,7 +317,7 @@ static int rx_ph_data_ind(struct osmocom_ms *ms, struct msgb *msg)
 			PRIM_OP_INDICATION, msg);
 	pp.u.data.chan_nr = dl->chan_nr;
 	pp.u.data.link_id = dl->link_id;
-	pp.u.data.fn = dl->frame_nr;
+	pp.u.data.fn = tm.fn;
 
 	/* send it up into LAPDm */
 	return lapdm_phsap_up(&pp.oph, le);