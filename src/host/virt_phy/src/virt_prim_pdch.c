osmocom/osmocom-bb

commit fa833e40956a72334174eea220e4b5a20bf7864a

l1gprs/l1ctl: Decouple RTS.ind from DL_BLOCK.ind
Before this patch, the RTS:ind was crafted up in the stack when
receiving the DL_BLOCK.ind. This created some problems since the
internal low level state has to be updated in between signalling
DL_BLOCK.ind and RTS.ind, as there's a fn-advnace of one block between
those 2 signals (hence the timeslot allocation has to be applied at the
time when the fn-advance is applied).
This is actually not fixing the whole issue, since there's several
timeslots and hence the following events will have the internal timeslot updated
during the event in the middle, hence potentially causing problems in the
remaining TS:
DL_BLOCK.ind(FN=N, TS=1),  RTS.ind(FN=N+4, TS=1), DL_BLOCK.ind(FN=N, TS=2)

In any case, this decoupling already improves the situation and is step
needed anyway towards fully fixing the problem (by, for instance,
maintaining a timeslot state duplicated both for DL and Ul directions,
since they drive based on differnet FN time (1 PDCH block).

Change-Id: I1494e0aac7555f6e01b4b435b77140afc42c098e

src/host/virt_phy/src/virt_prim_pdch.c
@@ -22,6 +22,7 @@
 #include <osmocom/core/utils.h>
 #include <osmocom/gsm/gsm_utils.h>
 #include <osmocom/gsm/protocol/gsm_08_58.h>
+#include <osmocom/gsm/gsm0502.h>
 
 #include <osmocom/bb/virtphy/l1ctl_sap.h>
 #include <osmocom/bb/virtphy/virt_l1_sched.h>
@@ -75,6 +76,7 @@ void l1ctl_tx_gprs_dl_block_ind(struct l1_model_ms *ms, const struct msgb *msg,
 {
 	struct l1gprs_prim_dl_block_ind ind;
 	struct msgb *nmsg;
+	uint8_t usf = 0xff;
 
 	if (ms->gprs == NULL)
 		return;
@@ -93,7 +95,14 @@ void l1ctl_tx_gprs_dl_block_ind(struct l1_model_ms *ms, const struct msgb *msg,
 		.data_len = msgb_length(msg),
 	};
 
-	nmsg = l1gprs_handle_dl_block_ind(ms->gprs, &ind);
+	nmsg = l1gprs_handle_dl_block_ind(ms->gprs, &ind, &usf);
 	if (nmsg != NULL)
+		l1ctl_sap_tx_to_l23_inst(ms, nmsg);
+	/* Every fn % 13 == 12 we have either a PTCCH or an IDLE slot, thus
+	 * every fn % 13 ==  8 we add 5 frames, or 4 frames othrwise.  The
+	 * resulting value is first fn of the next block. */
+	const uint32_t rts_fn = GSM_TDMA_FN_SUM(fn, (fn % 13 == 8) ? 5 : 4);
+	nmsg = l1gprs_handle_rts_ind(ms->gprs, rts_fn, tn, usf);
+	if (msg != NULL)
 		l1ctl_sap_tx_to_l23_inst(ms, nmsg);
 }

include/l1ctl_proto.h
@@ -59,6 +59,7 @@ enum {
 	L1CTL_GPRS_DL_BLOCK_IND		= 0x23,
 	/* Extended (11-bit) RACH (see 3GPP TS 05.02, section 5.2.7) */
 	L1CTL_EXT_RACH_REQ		= 0x24,
+	L1CTL_GPRS_RTS_IND		= 0x25,
 };
 
 enum ccch_mode {
@@ -385,6 +386,13 @@ struct l1ctl_gprs_dl_block_ind {
 	} meas;
 	uint8_t usf;
 	uint8_t data[0];
+} __attribute__((packed));
+
+/* payload of L1CTL_GPRS_RTS_IND */
+struct l1ctl_gprs_rts_ind {
+	uint32_t fn;
+	uint8_t tn;
+	uint8_t usf;
 } __attribute__((packed));
 
 #endif /* __L1CTL_PROTO_H__ */

include/l1gprs.h
@@ -112,4 +112,5 @@ int l1gprs_handle_ul_block_req(struct l1gprs_state *gprs,
 			       struct l1gprs_prim_ul_block_req *req,
 			       const struct msgb *msg);
 struct msgb *l1gprs_handle_dl_block_ind(struct l1gprs_state *gprs,
-					const struct l1gprs_prim_dl_block_ind *ind);
+					const struct l1gprs_prim_dl_block_ind *ind, uint8_t *usf);
+struct msgb *l1gprs_handle_rts_ind(struct l1gprs_state *gprs, uint32_t fn, uint8_t tn, uint8_t usf);

src/host/layer23/include/osmocom/bb/common/ms.h
@@ -43,6 +43,7 @@ struct osmosap_entity {
 struct osmol1_entity {
 	int (*l1_traffic_ind)(struct osmocom_ms *ms, struct msgb *msg);
 	int (*l1_gprs_dl_block_ind)(struct osmocom_ms *ms, struct msgb *msg);
+	int (*l1_gprs_rts_ind)(struct osmocom_ms *ms, struct msgb *msg);
 };
 
 struct osmomncc_entity {

src/host/layer23/include/osmocom/bb/modem/grr.h
@@ -25,6 +25,7 @@ enum grr_fsm_event {
 	GRR_EV_PDCH_DL_TBF_CFG_REQ,
 	GRR_EV_PDCH_BLOCK_REQ,
 	GRR_EV_PDCH_BLOCK_IND,
+	GRR_EV_PDCH_RTS_IND,
 };
 
 extern struct osmo_fsm grr_fsm_def;

src/host/layer23/src/common/l1ctl.c
@@ -986,6 +986,35 @@ static int rx_l1_gprs_dl_block_ind(struct osmocom_ms *ms, struct msgb *msg)
 	return 0;
 }
 
+/* Receive L1CTL_GPRS_RTS_IND */
+static int rx_l1_gprs_rts_ind(struct osmocom_ms *ms, struct msgb *msg)
+{
+	const struct l1ctl_gprs_rts_ind *ind = (void *)msg->l1h;
+
+	if (msgb_l1len(msg) < sizeof(*ind)) {
+		LOGP(DL1C, LOGL_ERROR,
+		     "Rx malformed GPRS RTS.ind (len=%u < %zu)\n",
+		     msgb_l1len(msg), sizeof(*ind));
+		return -EINVAL;
+	}
+	if (OSMO_UNLIKELY(ind->tn >= 8)) {
+		LOGP(DL1C, LOGL_ERROR,
+		     "Rx malformed GPRS RTS.ind (tn=%u)\n",
+		     ind->tn);
+		return -EINVAL;
+	}
+
+	DEBUGP(DL1C, "Rx RTS.ind (fn=%u, tn=%u, usf=%u)\n",
+	       ntohl(ind->fn), ind->tn, ind->usf);
+
+	/* distribute or drop */
+	if (ms->l1_entity.l1_gprs_rts_ind)
+		return ms->l1_entity.l1_gprs_rts_ind(ms, msg);
+
+	msgb_free(msg);
+	return 0;
+}
+
 /* Transmit L1CTL_GPRS_UL_BLOCK_REQ */
 int l1ctl_tx_gprs_ul_block_req(struct osmocom_ms *ms, uint32_t fn, uint8_t tn,
 			       const uint8_t *data, size_t data_len)
@@ -1131,6 +1160,9 @@ int l1ctl_recv(struct osmocom_ms *ms, struct msgb *msg)
 		break;
 	case L1CTL_GPRS_DL_BLOCK_IND:
 		rc = rx_l1_gprs_dl_block_ind(ms, msg);
+		break;
+	case L1CTL_GPRS_RTS_IND:
+		rc = rx_l1_gprs_rts_ind(ms, msg);
 		break;
 	default:
 		LOGP(DL1C, LOGL_ERROR, "Unknown MSG: %u\n", hdr->msg_type);

src/host/layer23/src/modem/grr.c
@@ -469,16 +469,15 @@ static void handle_pdch_block_ind(struct osmocom_ms *ms, struct msgb *msg)
 		}
 	};
 	osmo_gprs_rlcmac_prim_lower_up(prim);
+}
 
-	/* Do not send RTS.ind if we got PTCCH/D */
-	if (fn % 104 == 12)
-		return;
+static void handle_pdch_rts_ind(struct osmocom_ms *ms, struct msgb *msg)
+{
+	const struct l1ctl_gprs_rts_ind *ind = (void *)msg->l1h;
+	const uint32_t fn = osmo_load32be(&ind->fn);
+	struct osmo_gprs_rlcmac_prim *prim;
 
-	/* Every fn % 13 == 12 we have either a PTCCH or an IDLE slot, thus
-	 * every fn % 13 ==  8 we add 5 frames, or 4 frames othrwise.  The
-	 * resulting value is first fn of the next block. */
-	const uint32_t rts_fn = GSM_TDMA_FN_SUM(fn, (fn % 13 == 8) ? 5 : 4);
-	prim = osmo_gprs_rlcmac_prim_alloc_l1ctl_pdch_rts_ind(ind->hdr.tn, rts_fn, ind->usf);
+	prim = osmo_gprs_rlcmac_prim_alloc_l1ctl_pdch_rts_ind(ind->tn, fn, ind->usf);
 	osmo_gprs_rlcmac_prim_lower_up(prim);
 }
 
@@ -693,6 +692,9 @@ static void grr_st_packet_transfer_action(struct osmo_fsm_inst *fi,
 	case GRR_EV_PDCH_BLOCK_IND:
 		handle_pdch_block_ind(ms, (struct msgb *)data);
 		break;
+	case GRR_EV_PDCH_RTS_IND:
+		handle_pdch_rts_ind(ms, (struct msgb *)data);
+		break;
 	case GRR_EV_PDCH_RELEASE_REQ:
 		modem_sync_to_cell(ms);
 		osmo_fsm_inst_state_chg(fi, GRR_ST_PACKET_NOT_READY, 0, 0);
@@ -729,6 +731,7 @@ static const struct osmo_fsm_state grr_fsm_states[] = {
 				| S(GRR_EV_PDCH_DL_TBF_CFG_REQ)
 				| S(GRR_EV_PDCH_BLOCK_REQ)
 				| S(GRR_EV_PDCH_BLOCK_IND)
+				| S(GRR_EV_PDCH_RTS_IND)
 				| S(GRR_EV_PDCH_RELEASE_REQ),
 		.action = &grr_st_packet_transfer_action,
 		.onenter = &grr_st_packet_transfer_onenter,
@@ -747,6 +750,7 @@ static const struct value_string grr_fsm_event_names[] = {
 	OSMO_VALUE_STRING(GRR_EV_PDCH_DL_TBF_CFG_REQ),
 	OSMO_VALUE_STRING(GRR_EV_PDCH_BLOCK_REQ),
 	OSMO_VALUE_STRING(GRR_EV_PDCH_BLOCK_IND),
+	OSMO_VALUE_STRING(GRR_EV_PDCH_RTS_IND),
 	{ 0, NULL }
 };
 

src/host/layer23/src/modem/rlcmac.c
@@ -167,6 +167,11 @@ static int l1ctl_dl_block_cb(struct osmocom_ms *ms, struct msgb *msg)
 	return osmo_fsm_inst_dispatch(ms->grr_fi, GRR_EV_PDCH_BLOCK_IND, msg);
 }
 
+static int l1ctl_rts_cb(struct osmocom_ms *ms, struct msgb *msg)
+{
+	return osmo_fsm_inst_dispatch(ms->grr_fi, GRR_EV_PDCH_RTS_IND, msg);
+}
+
 int modem_rlcmac_init(struct osmocom_ms *ms)
 {
 	int rc;
@@ -182,6 +187,7 @@ int modem_rlcmac_init(struct osmocom_ms *ms)
 	osmo_gprs_rlcmac_prim_set_down_cb(modem_rlcmac_prim_down_cb, ms);
 
 	ms->l1_entity.l1_gprs_dl_block_ind = &l1ctl_dl_block_cb;
+	ms->l1_entity.l1_gprs_rts_ind = &l1ctl_rts_cb;
 
 	return rc;
 }

commit 9a71f0a18b5563844ec2cf4cd3d63549c5ffd9a4

virt_phy: fix wrong pointer being checked
Change-Id: I2f845b0335d2d9818aa15c30f8621acd120e82d4
Fixes: fa833e40 "l1gprs/l1ctl: Decouple RTS.ind from DL_BLOCK.ind"
Fixes: CID#323121

src/host/virt_phy/src/virt_prim_pdch.c
@@ -103,6 +103,6 @@ void l1ctl_tx_gprs_dl_block_ind(struct l1_model_ms *ms, const struct msgb *msg,
 	 * resulting value is first fn of the next block. */
 	const uint32_t rts_fn = GSM_TDMA_FN_SUM(fn, (fn % 13 == 8) ? 5 : 4);
 	nmsg = l1gprs_handle_rts_ind(ms->gprs, rts_fn, tn, usf);
-	if (msg != NULL)
+	if (nmsg != NULL)
 		l1ctl_sap_tx_to_l23_inst(ms, nmsg);
 }