cf32f3429270516828b560b6b5566f6522466bfb
Pau Espin Pedrol <pespin@sysmocom.de>
2024-02-02 16:52:41 +0100

    modem: Improve logging when forwarding data to GRR
    Change-Id: Idf80c0fa1c8293afbe354442a72f5b1c5a84f9e1

--- src/host/layer23/src/modem/llc.c
+++ src/host/layer23/src/modem/llc.c
@@ -142,7 +142,12 @@ int modem_llc_prim_down_cb(struct osmo_gprs_llc_prim *llc_prim, void *user_data)
 
 	switch (llc_prim->oph.sap) {
 	case OSMO_GPRS_LLC_SAP_GRR:
-		LOGP(DLLC, LOGL_DEBUG, "%s(): Rx %s ll=[%s]\n",  __func__, pdu_name,
+		LOGP(DLLC, LOGL_DEBUG, "%s(): Rx %s TLLI=0x%08x SAPI=%s RadioPrio=%u ll=[%s]\n",
+		     __func__, pdu_name, llc_prim->grr.tlli,
+		     /* data_req.sapi in same union pos: */
+		     osmo_gprs_llc_sapi_name(llc_prim->grr.unitdata_req.sapi),
+		     /* data_req.radio_prio in same union pos: */
+		     llc_prim->grr.unitdata_req.radio_prio,
 		     osmo_hexdump(llc_prim->grr.ll_pdu, llc_prim->grr.ll_pdu_len));
 		/* Forward it to lower layers, pass ownership over to RLCMAC: */
 		/* Optimization: LLC-GRR-UNITDATA-IND is 1-to-1 ABI compatible with
