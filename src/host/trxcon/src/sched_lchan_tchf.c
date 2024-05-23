osmocom/osmocom-bb

commit 7c00190b45205c2f1d2d8e708c1f6e9dd621ff69

trxcon/l1sched: fix sending dummy TCH/A[FH]S blocks
... in the absence of valid Uplink TCH/A[FH]S or FACCH frames.

Change-Id: I26205e8672cb2849ea79bd6206c1eff1172bd9d9
Related: libosmocore.git I82ce2adf995a4b42d1f378c5819f88d773b9104a
Related: OS#6049

src/host/trxcon/src/sched_lchan_tchf.c
@@ -305,13 +305,7 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 		const uint8_t *data = msg ? msgb_l2(msg) : NULL;
 		size_t data_len = msg ? msgb_l2len(msg) : 0;
 
-		if (msg == NULL) {
-			/* TODO: It's not clear what to do for TCH/AFS.
-			 * TODO: Send dummy FACCH maybe? */
-			goto send_burst; /* send something */
-		}
-
-		if (data_len != GSM_MACBLOCK_LEN) { /* TCH/AFS: speech */
+		if (msg != NULL && msg != msg_facch) { /* TCH/AFS: speech */
 			if (!l1sched_lchan_amr_prim_is_valid(lchan, msg, amr_fn_is_cmr))
 				goto free_bad_msg;
 			/* pull the AMR header - sizeof(struct amr_hdr) */
@@ -319,6 +313,7 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 			data += 2;
 		}
 
+		/* if msg == NULL, transmit a dummy speech block with inverted CRC6 */
 		rc = gsm0503_tch_afs_encode(BUFPOS(bursts_p, 0),
 					    data, data_len,
 					    amr_fn_is_cmr,

src/host/trxcon/src/sched_lchan_tchh.c
@@ -512,13 +512,7 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 		const uint8_t *data = msg ? msgb_l2(msg) : NULL;
 		size_t data_len = msg ? msgb_l2len(msg) : 0;
 
-		if (msg == NULL) {
-			/* TODO: It's not clear what to do for TCH/AHS.
-			 * TODO: Send dummy FACCH maybe? */
-			goto send_burst; /* send garbage */
-		}
-
-		if (data_len != GSM_MACBLOCK_LEN) { /* TCH/AHS: speech */
+		if (msg != NULL && msg != msg_facch) { /* TCH/AHS: speech */
 			if (!l1sched_lchan_amr_prim_is_valid(lchan, msg, amr_fn_is_cmr))
 				goto free_bad_msg;
 			/* pull the AMR header - sizeof(struct amr_hdr) */
@@ -526,6 +520,7 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 			data += 2;
 		}
 
+		/* if msg == NULL, transmit a dummy speech block with inverted CRC6 */
 		rc = gsm0503_tch_ahs_encode(BUFPOS(bursts_p, 0),
 					    data, data_len,
 					    amr_fn_is_cmr,

commit d70e8a6de72c361870bd9202110dac933d91992f

trxcon/l1sched: fix NULL pointer dereference in tx_tch[fh]_fn()
If msg is NULL, we're inducing a BFI condition at the BTS side receiver
by sending a TCH/A[FH]S block with invalid CRC6.  In this case we need
to skip the rest of the function and jump to send_burst immediately.

Change-Id: I159b2ed455377c77d8764f9320efd15333129afb
Fixes: 7c00190b "trxcon/l1sched: fix sending dummy TCH/A[FH]S blocks"
Fixes: CID#368538

src/host/trxcon/src/sched_lchan_tchf.c
@@ -321,6 +321,8 @@ int tx_tchf_fn(struct l1sched_lchan_state *lchan,
 					    lchan->amr.codecs,
 					    lchan->amr.ul_ft,
 					    lchan->amr.ul_cmr);
+		if (msg == NULL)
+			goto send_burst;
 		break;
 	}
 	/* CSD (TCH/F14.4): 14.5 kbit/s radio interface rate */

src/host/trxcon/src/sched_lchan_tchh.c
@@ -528,6 +528,8 @@ int tx_tchh_fn(struct l1sched_lchan_state *lchan,
 					    lchan->amr.codecs,
 					    lchan->amr.ul_ft,
 					    lchan->amr.ul_cmr);
+		if (msg == NULL)
+			goto send_burst;
 		break;
 	}
 	/* CSD (TCH/H4.8): 6.0 kbit/s radio interface rate */