cb0a7293e6e9e80b51a81136e88e747858cbd6bc
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2024-09-16 01:18:00 +0700

    firmware: k2xx: fix comments about flash organization
    Change-Id: I1b6a50cf7e10b5d71c6aa8d492721a7345ad892a

--- src/target/firmware/board/se_k2xx/init.c
+++ src/target/firmware/board/se_k2xx/init.c
@@ -132,9 +132,9 @@ void board_init(int with_irq)
 	/* Initialize ABB driver (uses SPI) */
 	twl3025_init();
 
-	/* K200i uses 13 sectors of 256 KiB each */
+	/* SPANSION flash has 13 sectors of 256 KiB each */
 	if (tiffs_init(0x01800000, 0x40000, 13) < 0) {
-		/* K220i uses 52 sectors of 64 KiB each */
+		/* SAMSUNG flash has 52 sectors of 64 KiB each */
 		tiffs_init(0x01800000, 0x10000, 52);
 	}
 }
