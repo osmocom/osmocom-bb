54a0052ab8de1639c306cb3afd1f0ce19b8e2c9c
Pau Espin Pedrol <pespin@sysmocom.de>
2025-03-18 23:55:09 +0100

    fake_trx: Remove unneeded field assignment
    The assignment is not really needed, since worker can already access the
    field anyway.
    
    Fixes: 6f45d36e936c9209a18e02b6a8c3a04eb1ff9fd9
    Change-Id: Ib8808f8bf8f3ef084334debac47ca48a48aa0cea

--- src/target/trx_toolkit/clck_gen.py
+++ src/target/trx_toolkit/clck_gen.py
@@ -70,7 +70,6 @@ class CLCKGen:
 		# Initialize and start a new thread
 		self._thread = threading.Thread(target = self._worker)
 		self._thread.daemon = True
-		self._thread.sched_rr_prio = self.sched_rr_prio
 		self._thread.start()
 
 	def stop(self):
