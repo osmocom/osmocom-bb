osmocom/osmocom-bb

commit c6fe9c3fa125e7783b42885bff8fcfda2ced708a

trxcon: trxcon_fsm: permit loop [P]DCH transitions
According to 3GPP TS 44.004, Figure 5.1, it's absolutely legal to
perform loop transitions in state 'DCH' (i.e. from 'DCH' to 'DCH').

This kind of transition is needed in the following cases:

* on reciept of RR FREQUENCY REDEFINITION,
* on reciept of RR ASSIGNMENT REQUEST,
* on reciept of RR HANDOVER REQUEST,

when going back from state 'DCH' to state 'BCH' is not required nor
actually desired.  In context of the trxcon_fsm, this applies to
both TRXCON_ST_DEDICATED and TRXCON_ST_PACKET_DATA, as they both
represent what's defined as 'DCH' in the Figure 5.1.

Change-Id: I3b402ec84610a5df744d9b06e5f7dab7a9a3ddad
Related: OS#5599

src/host/trxcon/src/trxcon_fsm.c
@@ -336,6 +336,9 @@ static void handle_dch_est_req(struct osmo_fsm_inst *fi,
 		trxcon->l1p.band_arfcn = req->h0.band_arfcn;
 	}
 
+	/* Remove all active timeslots */
+	l1sched_reset(trxcon->sched, false);
+
 	rc = l1sched_configure_ts(trxcon->sched, req->chan_nr & 0x07, config);
 	if (rc)
 		return;
@@ -409,6 +412,9 @@ static void trxcon_st_dedicated_action(struct osmo_fsm_inst *fi,
 		break;
 	case TRXCON_EV_TX_ACCESS_BURST_CNF:
 		l1ctl_tx_rach_conf(trxcon, (const struct trxcon_param_tx_access_burst_cnf *)data);
+		break;
+	case TRXCON_EV_DCH_EST_REQ:
+		handle_dch_est_req(fi, (const struct trxcon_param_dch_est_req *)data);
 		break;
 	case TRXCON_EV_DCH_REL_REQ:
 		l1sched_reset(trxcon->sched, false);
@@ -530,6 +536,9 @@ static void trxcon_st_packet_data_action(struct osmo_fsm_inst *fi,
 			LOGPFSML(fi, LOGL_NOTICE, "Rx PTCCH/D message\n");
 		break;
 	}
+	case TRXCON_EV_DCH_EST_REQ:
+		handle_dch_est_req(fi, (const struct trxcon_param_dch_est_req *)data);
+		break;
 	case TRXCON_EV_DCH_REL_REQ:
 		l1sched_reset(trxcon->sched, false);
 		osmo_fsm_inst_state_chg(fi, TRXCON_ST_RESET, 0, 0);
@@ -601,8 +610,10 @@ static const struct osmo_fsm_state trxcon_fsm_states[] = {
 	[TRXCON_ST_DEDICATED] = {
 		.name = "DEDICATED",
 		.out_state_mask = S(TRXCON_ST_RESET)
-				| S(TRXCON_ST_FBSB_SEARCH),
+				| S(TRXCON_ST_FBSB_SEARCH)
+				| S(TRXCON_ST_DEDICATED),
 		.in_event_mask  = S(TRXCON_EV_DCH_REL_REQ)
+				| S(TRXCON_EV_DCH_EST_REQ)
 				| S(TRXCON_EV_TX_ACCESS_BURST_REQ)
 				| S(TRXCON_EV_TX_ACCESS_BURST_CNF)
 				| S(TRXCON_EV_SET_TCH_MODE_REQ)
@@ -615,8 +626,10 @@ static const struct osmo_fsm_state trxcon_fsm_states[] = {
 	[TRXCON_ST_PACKET_DATA] = {
 		.name = "PACKET_DATA",
 		.out_state_mask = S(TRXCON_ST_RESET)
-				| S(TRXCON_ST_FBSB_SEARCH),
+				| S(TRXCON_ST_FBSB_SEARCH)
+				| S(TRXCON_ST_PACKET_DATA),
 		.in_event_mask  = S(TRXCON_EV_DCH_REL_REQ)
+				| S(TRXCON_EV_DCH_EST_REQ)
 				| S(TRXCON_EV_TX_ACCESS_BURST_REQ)
 				| S(TRXCON_EV_TX_ACCESS_BURST_CNF)
 				| S(TRXCON_EV_RX_DATA_IND),

commit dd5c263ae05795966435b9d1a1e563957db793b8

trxcon/fsm: handle_dch_est_req(): do not reset the scheduler
Calling l1sched_reset() results in resetting the UL SACCH cache,
that may have been populated prior to sending L1CTL_DM_EST_REQ.
Instead, do what the comment says - call l1sched_del_all_ts().

Change-Id: Iff03fa5b90ac1ec30b7937cea6bd09c6ececb5f6
Fixes: c6fe9c3f ("trxcon: trxcon_fsm: permit loop [P]DCH transitions")
Related: 15877ba05 ("trxcon: allow populating global SACCH cache via L1CTL")
Related: OS#6919

src/host/trxcon/src/trxcon_fsm.c
@@ -352,7 +352,7 @@ static void handle_dch_est_req(struct osmo_fsm_inst *fi,
 	}
 
 	/* Remove all active timeslots */
-	l1sched_reset(trxcon->sched);
+	l1sched_del_all_ts(trxcon->sched);
 
 	rc = l1sched_configure_ts(trxcon->sched, req->chan_nr & 0x07, config);
 	if (rc)